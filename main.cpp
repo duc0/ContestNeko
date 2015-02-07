83cbc3c19d4036459a24a695bd842d6eda3dcfde
duc <ducnm0@gmail.com>
2015-02-07 09:29:59 -0800

    Solution

--- main.cpp
+++ main.cpp
@@ -22,100 +22,52 @@ using namespace std;
 
 
 
-
-#define MAXN 400
-
-#define MOD 1000000007
-#define MAXT 101000
-
-int n, q, t;
-int a[MAXN];
-int more[MAXN];
-int mless[MAXN];
-
-int f[MAXT];
-
 void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  n = 300;
-  q = 299;
-  t = 100000;
-  cout << n << " " << q << " " << t << endl;
-  for (int i = 0; i < n; ++i) {
-    cout << 1 << " ";
-  }
-  cout << endl;
-  for (int i = 0; i < q; ++i) {
-    cout << i + 1 << " " << i + 2 << endl;
-  }
-  fclose(stdout);
 }
+
+#define MAXN 70
+int n, need;
+int a[MAXN];
+int ret[MAXN];
 
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  freopen("input4.txt", "r", stdin);
   
-  cin >> n;
-  cin >> q;
-  cin >> t;
+  cin >> n >> need;
   for (int i = 1; i <= n; ++i) {
-    cin >> a[i];
+    a[i] = i;
   }
-  int b, c;
-  fill0(more);
-  fill0(mless);
-  repeat(q) {
-    cin >> b >> c;
-    more[b] = c;
-    mless[c] = b;
-  }
-  for (int i = 1; i <= n; ++i) {
-    int j = i;
-    while (more[j] != 0) {
-      j = more[j];
-      if (j == i) {
-        // Cycle!
-        cout << 0 << endl;
-        return 0;
+  int bestS = 0;
+  int idx = 0;
+  do {
+    int s = 0;
+    for (int i = 1; i <= n; ++i) {
+      int m = a[i];
+      for (int j = i; j <= n; ++j) {
+        if (a[j] < m) {
+          m = a[j];
+        }
+        s += m;
       }
     }
-  }
-  for (int i = 1; i <= n; ++i) {
-    if (mless[i] != 0) {
-      continue;
+    if (s > bestS) {
+      bestS = s;
+      idx = 1;
+    } else if (s == bestS) {
+      idx++;
     }
-    int j = i;
-    while (more[j] != 0) {
-      // j, more[j]
-      a[more[j]] += a[j]; // reduce x[j] > x[more[j]] => x[j] > 0
-      t -= a[j]; // reduce x[j] > 0 => x[j] >= 0
-      // Overflow can happen, so we should check here!
-      if (t < 0) {
-        cout << 0 << endl;
-        return 0;
-      }
-      j = more[j];
-    }
-  }
-  
-  if (t == 0) {
-    cout << 1 << endl;
-    return 0;
-  }
-  
-  assert(t > 0);
-  
-  memset(f, 0, sizeof(f));
-  for (int i = 1; i <= n; ++i) {
-    f[0] = 1;
-    for (int iterT = 1; iterT <= t; ++iterT) {
-      if (iterT >= a[i]) {
-        f[iterT] = (f[iterT] + f[iterT - a[i]]) % MOD;
+    if (s == bestS) {
+      if (idx == need) {
+        for (int i = 1; i <= n; ++i) {
+          ret[i] = a[i];
+        }
       }
     }
+  } while (next_permutation(a + 1, a + n + 1));
+  //cout << bestS << endl;
+  for (int i = 1; i <= n; ++i) {
+    cout << ret[i] << " ";
   }
-  
-  cout << f[t] << endl;
-  
   return 0;
 }
