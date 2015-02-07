94fcc35e35ff362111739fd32d0399df5f2a29bf
duc <ducnm0@gmail.com>
2015-02-07 15:43:35 -0800

    A N^6*K TLE sol

--- main.cpp
+++ main.cpp
@@ -26,65 +26,62 @@ void testGen() {
 }
 
 #define MAXN 110
-int p[MAXN], n, k;
+int p[MAXN], n, k, pos[MAXN];
+int totalOps;
 
-int countInv[MAXN*(MAXN - 1) / 2], countAll;
+double pr[2][MAXN][MAXN]; // pr[i][j] = prob that p[i] = x and p[j] = y after k operations
 
-int countInversion() {
-  int cnt = 0;
-  for (int i = 1; i < n; ++i) {
-    for (int j = i + 1; j <= n; ++j) {
-      if (p[i] > p[j]) {
-        cnt++;
-      }
-    }
+int getPos(int i, int l, int r) { // get pos of p[i] after reverse(l, r)
+  if (i < l || i > r) {
+    return i;
   }
-  return cnt;
-}
-
-void reverse(int l, int r) {
-  int mid = (l + r) / 2;
-  
-  int ii = l, jj = r;
-  for (int i = l; i <= mid; ++i) {
-    swap(p[ii], p[jj]);
-    ii++;
-    jj--;
-  }
-}
-
-void bruteforce(int numOp) {
-  if (numOp == 0) {
-    int ret = countInversion();
-    countInv[ret]++;
-    countAll++;
-    return;
-  }
-  for (int l = 1; l <= n; ++l) {
-    for (int r = l; r <= n; ++r) {
-      reverse(l, r);
-      bruteforce(numOp - 1);
-      reverse(l, r);
-    }
-  }
+  return r - i + l;
 }
 int main() {
   //testGen();
-  //freopen("input3.txt", "r", stdin);
+  freopen("input2.txt", "r", stdin);
   
   cin >> n >> k;
   for (int i = 1; i <= n; ++i) {
     cin >> p[i];
+    pos[p[i]] = i;
   }
-  
-  countAll = 0;
-  fill0(countInv);
-  bruteforce(k);
+  totalOps = n * (n + 1) / 2;
+  double probOp = (double) 1 / totalOps;
   
   double ret = 0;
-  for (int i = 0; i <= n * (n - 1) / 2; ++i) {
-    ret += i * ((double)countInv[i] / countAll);
+  
+  for (int x = 1; x < n; ++x) {
+    for (int y = x + 1; y <= n; ++y) {
+      fill0(pr);
+      int cur = 0, next = 1;
+      pr[cur][pos[x]][pos[y]] = 1;
+      repeat(k) {
+        fill0(pr[next]);
+        for (int i = 1; i <= n; ++i) {
+          for (int j = 1; j <= n; ++j) {
+            if (i == j) continue;
+            if (pr[cur][i][j] == 0) continue;
+            for (int l = 1; l <= n; ++l) {
+              for (int r = l; r <= n; ++r) {
+                int i2 = getPos(i, l, r);
+                int j2 = getPos(j, l, r);
+                pr[next][i2][j2] += pr[cur][i][j] * probOp;
+              }
+            }
+          }
+        }
+        cur = 1 - cur;
+        next = 1 -  next;
+      }
+      for (int py = 1; py < n; ++py) {
+        for (int px = py + 1; px <= n; ++px) {
+          ret += pr[cur][px][py];
+        }
+      }
+    }
   }
+  
   printf("%.10lf", ret);
   return 0;
 }
