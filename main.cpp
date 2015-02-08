b3f4afd72866e4407362644d5fea3b25721e4a58
duc <ducnm0@gmail.com>
2015-02-07 17:57:47 -0800

    Add big sol (still constant factor slow)

--- main.cpp
+++ main.cpp
@@ -19,9 +19,14 @@ using namespace std;
 #define int64 long long
 #define repeat(x) for(auto repeat_var=0;repeat_var<x;++repeat_var)
 #define fill0(x) memset(x, 0, sizeof(x))
+#define fillneg(x) memset(x, -1, sizeof(x))
 
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
+  cout << 100 << " " << 100000 << endl;
+  for (int i = 1; i <= 100; ++i) {
+    cout << i << " ";
+  }
   fclose(stdout);
 }
 
@@ -66,55 +71,67 @@ int countOpSwap2(int i, int j, int j2) {
   return min(j2 - i, n - j + 1);
 }
 
+int countOpNotAffect[MAXN][MAXN];
+
 int main() {
   //testGen();
-  freopen("input3.txt", "r", stdin);
+  freopen("biginput1.txt", "r", stdin);
   
   cin >> n >> k;
+  if (k > 1000) {
+    k = 1000;
+  }
   for (int i = 1; i <= n; ++i) {
     cin >> p[i];
     pos[p[i]] = i;
   }
   totalOps = n * (n + 1) / 2;
   
+  double probOp = (double)1 / totalOps;
+  
   fill0(pr);
   int cur = 0, next = 1;
   for (int i = 1; i < n; ++i) {
     for (int j = i + 1; j <= n; ++j) {
       pr[cur][i][j] = p[i] > p[j] ? 1 : 0;
+      
+      countOpNotAffect[i][j] = (i-1) * i / 2 + (n - j) * (n - j + 1) / 2 + (j - i - 1) * (j - i) / 2;
     }
   }
   
+  for (int i = 1; i < n; ++i) {
+    for (int j = i + 1; j <= n; ++j) {
+      
+    }
+  }
   repeat(k) {
     fill0(pr[next]);
     for (int i = 1; i < n; ++i) {
       for (int j = i + 1; j <= n; ++j) {
+        double curp = pr[cur][i][j];
         for (int i2 = 1; i2 < j; ++i2) {
           int cnt = countOpSwap1(i, j, i2);
-          //cout << i << " " << j << " " << i2 << " " << cnt << endl;
-          pr[next][i2][j] += pr[cur][i][j] * ((double) cnt / totalOps);
+          pr[next][i2][j] += curp * cnt;
         }
         for (int j2 = i + 1; j2 <= n; ++j2) {
           int cnt = countOpSwap2(i, j, j2);
-          //cout << i << " " << j << " " << j2 << " " << cnt << endl;
-          pr[next][i][j2] += pr[cur][i][j] * ((double) cnt / totalOps);
+          pr[next][i][j2] += curp * cnt;
         }
-        pr[next][i][j] += ((double)(i-1) * i / 2 + (n - j) * (n - j + 1) / 2 + (j - i - 1) * (j - i) / 2)  * pr[cur][i][j] / totalOps;
-        for (int j2 = 1; j2 <= n; ++j2) {
-          int i2 = j2 + (j - i);
-          if (i2 > n) break;
+        pr[next][i][j] += countOpNotAffect[i][j] * pr[cur][i][j];
+        for (int j2 = 1, i2 = j2 + j - i; j2 <= n && i2 <= n; ++j2, ++i2) {
           int sum_r_l = j2 + j;
           int cnt = countOp(sum_r_l, i, j);
-          assert(j2 < i2);
-          //cout << i << " " << j << " " << i2 << " " << j2 << endl;
-          //cout << sum_r_l << " " << i << " " << j << " " << cnt << endl;
-          pr[next][j2][i2] += (1 - pr[cur][i][j]) * ((double) cnt/ totalOps);
+          pr[next][j2][i2] += (1 - curp) * cnt;
         }
+      }
+    }
+    for (int i = 1; i < n; ++i) {
+      for (int j = i + 1; j <= n; ++j) {
+        pr[next][i][j] *= probOp;
       }
     }
     cur = 1 - cur;
     next = 1 - next;
-    //cout << pr[cur][1][2] <<  " " << pr[cur][2][3] << " " << pr[cur][1][3] << endl;
   }
   
   double ret = 0;
