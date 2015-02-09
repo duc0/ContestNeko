53cd5431a6ae375c25072778b392802fd5ccef37
duc <ducnm0@gmail.com>
2015-02-08 16:17:49 -0800

    Sol for EBig

--- main.cpp
+++ main.cpp
@@ -19,14 +19,10 @@ using namespace std;
 #define int64 long long
 #define repeat(x) for(auto repeat_var=0;repeat_var<x;++repeat_var)
 #define fill0(x) memset(x, 0, sizeof(x))
+#define INF_INT 2E9L
 
-#define MAXN 500
-#define MAXK 60
-
-int n, k;
-int a[MAXN];
-int f[MAXN][MAXN][MAXK];
-int sumPart[MAXN];
+#define MAXN 30100
+#define MAXK 210
 
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
@@ -37,60 +33,50 @@ void testGen() {
   fclose(stdout);
 }
 
-int getSum(int l, int r) {
-  return sumPart[r] - sumPart[l - 1];
-}
+int n, k;
+int a[MAXN];
+int f[2][2][MAXN][MAXK];
 
 int main() {
   //testGen();
-  //freopen("biginput1.txt", "r", stdin);
+  //freopen("input1.txt", "r", stdin);
   
   cin >> n >> k;
-  sumPart[0] = 0;
   for (int i = 1; i <= n; ++i) {
     cin >> a[i];
-    sumPart[i] = a[i] + sumPart[i - 1];
   }
   
+  // Let s1, s2, ..., sk be the sum of k subarrays
+  // Relax the problem to optimize
+  // g(s) = (qi + pi) * si, such that pi, qi are -1 or 1, and:
+  // q1 = 0
+  // pk = 0
+  // pi = - q(i + 1), for i < k
+  // We can easily shown that g(s) is also equal to max |s1 - s2| + ... + |s(k -1) - sk|
   fill0(f);
-  for (int last = 1; last <= n; ++last) {
-    for (int prev = 1; prev <= last; ++prev) {
-      for (int numSeq = 1; numSeq <= k; ++numSeq) {
-        if (numSeq > last) {
-          continue;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= k; ++j) {
+      for (int p = 0; p < 2; ++p) {
+        for (int q = 0; q < 2; ++q) {
+          f[p][q][i][j] = -INF_INT;
+          if (i < j) continue;
+          int qq = j == 1 ? 0 : (2 * q - 1);
+          int pp = j == k ? 0 : (2 * p  - 1);
+          f[p][q][i][j] = (pp + qq) * a[i] + max(f[p][q][i - 1][j], j > 1 ? max(f[1 - q][0][i - 1][j - 1], f[1 - q][1][i - 1][j - 1]) : 0);
         }
-        if (numSeq - 1 > prev - 1) {
-          continue;
-        }
-        int sumCur = getSum(prev, last);
-        if (numSeq == 1) {
-          f[last][prev][numSeq] = 0;
-          continue;
-        }
-        int best = 0;
-        for (int before = 1; before < prev; ++before) {
-          int sumPrev = getSum(before, prev - 1);
-          if (numSeq - 2 > before - 1) {
-            continue;
-          }
-          int cand = abs(sumCur - sumPrev) + f[prev - 1][before][numSeq - 1];
-          if (cand > best) {
-            best = cand;
-          }
-        }
-        f[last][prev][numSeq] = best;
       }
     }
   }
   
-  int best = 0;
+  int ret = 0;
   for (int i = 1; i <= n; ++i) {
-    for (int prev = 1; prev <= n; ++prev) {
-      if (k <= i && f[i][prev][k] > best) {
-        best = f[i][prev][k];
+    for (int p = 0; p < 2; ++p) {
+      for (int q = 0; q < 2; ++q) {
+        ret = max(ret, f[p][q][i][k]);
       }
     }
   }
-  cout << best << endl;
+  cout << ret << endl;
+  
   return 0;
 }
