8746c879c132fc5a1cfa1abb15f2418b5ba6b838
duc <ducnm0@gmail.com>
2015-02-10 08:22:59 -0800

    Solution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cstring>
 
-//#define NDEBUG
+#define NDEBUG
 #include <cassert>
 
 #include <iostream>
@@ -13,170 +13,32 @@
 #include <cstdlib>
 #include <array>
 #include <type_traits>
-#include <queue>
 
 using namespace std;
 
 #define int64 long long
 #define repeat(x) for(auto repeat_var=0;repeat_var<x;++repeat_var)
 #define fill0(x) memset(x, 0, sizeof(x))
-#define INT_INF 2E9L
 
-#define ntype int
-#define N_INF INT_INF
-
-struct Network {
-  struct Edge {int u, v; ntype f, c;};
-  vector<Edge> edgeList;
-  vector<vector<int> > adj;
-  void addEdge(int u, int v, ntype c) {
-    Edge e={u,v,0,c};
-    adj[u].push_back((int)edgeList.size());
-    adj[v].push_back((int)edgeList.size());
-    edgeList.push_back(e);
-  }
-  int n(){return (int)adj.size();}
-  void resetFlow() {
-    for (auto &pe : edgeList) pe.f=0;
-  }
-  void init(int n) {
-    edgeList.clear();
-    adj.resize(n);
-    for (auto &au : adj) au.clear();
-  }
-  Network(int n){init(n);}
-};
-
-class Dinic {
-  Network &g;
-  int s, t;
-  vector<ntype> dist;
-  vector<bool> block;
-  bool computeDist() {
-    queue<int> q;
-    q.push(t);
-    fill(dist.begin(),dist.end(),-1);
-    dist[t]=0;
-    while (!q.empty()) {
-      int u=q.front();
-      q.pop();
-      for (auto pi : g.adj[u]) {
-        Network::Edge &e=g.edgeList[pi];
-#define CONSIDER(v) {if (dist[v]==-1) {dist[v]=dist[u]+1; q.push(v); if (v==s) return true;}}
-        if (e.v==u && e.f<e.c) CONSIDER(e.u) else if (e.u==u && e.f>0) CONSIDER(e.v)
-#undef CONSIDER
-          }
-    }
-    return dist[s]!=-1;
-  }
-  ntype findAugmentingPath(int u, ntype delta) {
-    if (u==t) return delta;
-    ntype inc;
-    for (auto pi : g.adj[u]){
-      Network::Edge &e=g.edgeList[pi];
-#define CONSIDER(v,i,d) {if (!block[v] && dist[u]==dist[v]+1 && (inc=findAugmentingPath(v,min(delta,d)))) {e.f+=i*inc; return inc;}}
-      if (e.u==u && e.f<e.c) CONSIDER(e.v,1,e.c-e.f) else if (e.v==u && e.f>0) CONSIDER(e.u,-1,e.f)
-#undef CONSIDER
-        }
-    block[u]=true;
-    return 0;
-  }
+class Decipherability {
 public:
-  ntype totalFlow;
-  Dinic(Network &g, int s, int t):g(g),s(s),t(t){
-    g.resetFlow();
-    totalFlow=0;
-    dist.resize(g.n());
-    block.resize(g.n());
-  }
-  ntype run () {
-    while (computeDist()) {
-		    fill(block.begin(), block.end(), false);
-		    while (ntype inc = findAugmentingPath(s, N_INF)) totalFlow += inc;
-    }
-    return totalFlow;
-  }
-};
-
-class DoubleTree {
-  int n;
-  vector<int> p1, p2;
-  
-  vector< vector<int >> next1, next2;
-  vector<bool> visit;
-  
-  void dfs1(int u) {
-    visit[u] = true;
-    for (auto &v: next1[u]) {
-      if (!visit[v]) {
-        p1[v] = u;
-        dfs1(v);
-      }
-    }
-  }
-  
-  void dfs2(int u) {
-    visit[u] = true;
-    for (auto &v: next2[u]) {
-      if (!visit[v]) {
-        p2[v] = u;
-        dfs2(v);
-      }
-    }
-  }
-  
-  void makeRoot(int root) {
-    fill(visit.begin(), visit.end(), false);
-    dfs1(root);
-    fill(visit.begin(), visit.end(), false);
-    dfs2(root);
-  }
-  
-public:
-  int maximalScore( vector <int> a, vector <int> b, vector <int> c, vector <int> d, vector <int> score ) {
-    n = (int) a.size() + 1;
-    
-    assert((int)a.size() == n-1);
-    assert((int)b.size() == n-1);
-    
-    p1.resize(n);
-    p2.resize(n);
-    visit.resize(n);
-    
-    next1.resize(n);
-    next2.resize(n);
-    
-    for (int i = 0; i < n - 1; ++i) {
-      next1[a[i]].push_back(b[i]);
-      next2[a[i]].push_back(b[i]);
-    }
-    
-    int best = -INT_INF;
-    int MVAL = 1001;
-    for (int i = 0; i < n; ++i) {
-      makeRoot(i);
-      
-      Network g(2 * n + 2);
-      
-      int s = 2 * n;
-      int t = 2 * n + 1;
-      for (int u = 0; u < n; ++u) {
-        g.addEdge(s, u, 0 + MVAL);
-        g.addEdge(u, t, score[u] + MVAL);
-        if (u != i) {
-          g.addEdge(u, p1[u], INT_INF);
-          g.addEdge(u, p2[u], INT_INF);
-        }
-      }
-      int maxFlow = Dinic(g, s, t).run();
-      int ret = maxFlow  / (MVAL * n);
-      if (ret > best) {
-        best = ret;
-      }
-    }
-    
-    return best;
-  }
+   string check( string s, int k ) {
+     int n = (int) s.length();
+     k = n - k;
+     if (k == 0) {
+       return "Certain";
+     }
+     for (int i = 0; i < n; ++i) {
+       for (int j = i + 1; j < n; ++j) {
+         if (s[i] == s[j]) {
+           if (i + n - 1 - j >= k - 1) {
+             return "Uncertain";
+           }
+         }
+       }
+     }
+     return "Certain";
+   }
 };
 
 // BEGIN CUT HERE
@@ -217,7 +79,7 @@ namespace moj_harness {
 		}
 	}
 	
-	int verify_case(int casenum, const int &expected, const int &received, std::clock_t elapsed) { 
+	int verify_case(int casenum, const string &expected, const string &received, std::clock_t elapsed) { 
 		std::cerr << "Example " << casenum << "... "; 
 		
 		string verdict;
@@ -247,8 +109,8 @@ namespace moj_harness {
 		std::cerr << std::endl;
 		
 		if (verdict == "FAILED") {
-			std::cerr << "    Expected: " << expected << std::endl; 
-			std::cerr << "    Received: " << received << std::endl; 
+			std::cerr << "    Expected: \"" << expected << "\"" << std::endl; 
+			std::cerr << "    Received: \"" << received << "\"" << std::endl; 
 		}
 		
 		return verdict == "PASSED";
@@ -257,114 +119,96 @@ namespace moj_harness {
 	int run_test_case(int casenum__) {
 		switch (casenum__) {
 		case 0: {
-			int a[]                   = {0,0,1};
-			int b[]                   = {1,3,2};
-			int c[]                   = {0,0,3};
-			int d[]                   = {1,3,2};
-			int score[]               = {1000,24,100,-200};
-			int expected__            = 1024;
+			string s                  = "snuke";
+			int K                     = 2;
+			string expected__         = "Certain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 		case 1: {
-			int a[]                   = {0,0,1};
-			int b[]                   = {1,3,2};
-			int c[]                   = {0,0,3};
-			int d[]                   = {1,3,2};
-			int score[]               = {1000,24,100,200};
-			int expected__            = 1324;
+			string s                  = "aba";
+			int K                     = 1;
+			string expected__         = "Certain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 		case 2: {
-			int a[]                   = {0,0,1};
-			int b[]                   = {1,3,2};
-			int c[]                   = {0,0,3};
-			int d[]                   = {1,3,2};
-			int score[]               = {-1000,-24,-100,-200};
-			int expected__            = 0;
+			string s                  = "aba";
+			int K                     = 2;
+			string expected__         = "Uncertain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 		case 3: {
-			int a[]                   = {0,0,1};
-			int b[]                   = {1,3,2};
-			int c[]                   = {0,0,3};
-			int d[]                   = {1,3,2};
-			int score[]               = {-1000,24,100,200};
-			int expected__            = 200;
+			string s                  = "abcdabcd";
+			int K                     = 3;
+			string expected__         = "Certain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 		case 4: {
-			int a[]                   = {0,0,1,1,2,2};
-			int b[]                   = {1,2,3,4,5,6};
-			int c[]                   = {0,0,1,1,2,2};
-			int d[]                   = {1,2,3,4,5,6};
-			int score[]               = {-3,2,2,-1,2,2,-1};
-			int expected__            = 5;
+			string s                  = "koukyoukoukokukikou";
+			int K                     = 2;
+			string expected__         = "Uncertain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 		case 5: {
-			int a[]                   = {0,0,1,1,2,2};
-			int b[]                   = {1,2,3,4,5,6};
-			int c[]                   = {0,0,0,0,0,0};
-			int d[]                   = {1,2,3,4,5,6};
-			int score[]               = {-3,2,2,-1,2,2,-1};
-			int expected__            = 5;
+			string s                  = "wolfsothe";
+			int K                     = 8;
+			string expected__         = "Uncertain";
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 6: {
+			string s                  = "aa";
+			int K                     = 2;
+			string expected__         = "Certain";
+
+			std::clock_t start__      = std::clock();
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}
 
 		// custom cases
 
-/*      case 6: {
-			int a[]                   = ;
-			int b[]                   = ;
-			int c[]                   = ;
-			int d[]                   = ;
-			int score[]               = ;
-			int expected__            = ;
-
-			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
-			return verify_case(casenum__, expected__, received__, clock()-start__);
-		}*/
 /*      case 7: {
-			int a[]                   = ;
-			int b[]                   = ;
-			int c[]                   = ;
-			int d[]                   = ;
-			int score[]               = ;
-			int expected__            = ;
+			string s                  = ;
+			int K                     = ;
+			string expected__         = ;
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}*/
 /*      case 8: {
-			int a[]                   = ;
-			int b[]                   = ;
-			int c[]                   = ;
-			int d[]                   = ;
-			int score[]               = ;
-			int expected__            = ;
+			string s                  = ;
+			int K                     = ;
+			string expected__         = ;
 
 			std::clock_t start__      = std::clock();
-			int received__            = DoubleTree().maximalScore(vector <int>(a, a + (sizeof a / sizeof a[0])), vector <int>(b, b + (sizeof b / sizeof b[0])), vector <int>(c, c + (sizeof c / sizeof c[0])), vector <int>(d, d + (sizeof d / sizeof d[0])), vector <int>(score, score + (sizeof score / sizeof score[0])));
+			string received__         = Decipherability().check(s, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 9: {
+			string s                  = ;
+			int K                     = ;
+			string expected__         = ;
+
+			std::clock_t start__      = std::clock();
+			string received__         = Decipherability().check(s, K);
 			return verify_case(casenum__, expected__, received__, clock()-start__);
 		}*/
 		default:
@@ -376,6 +220,7 @@ namespace moj_harness {
 
 #include <cstdlib>
 int main(int argc, char *argv[]) {
+moj_harness::run_test(6);
 	if (argc == 1) {
 		moj_harness::run_test();
 	} else {
