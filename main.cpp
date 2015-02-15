593b55651e7e7fab23c51598d3d45f351727c81f
duc <ducnm0@gmail.com>
2015-02-15 09:15:43 -0800

    Add more headers

--- main.cpp
+++ main.cpp
@@ -13,13 +13,25 @@
 #include <cstdlib>
 #include <array>
 #include <type_traits>
+#include <queue>
+#include <stack>
+#include <functional>
 
 using namespace std;
 
 #define int64 long long
 #define repeat(x) for(auto repeat_var=0;repeat_var<x;++repeat_var)
+
+#define for_inc(i,x) for(auto i=0; i < x;++i)
+#define for_dec(i,x) for(auto i=x-1; i >= 0; --i)
+#define for_inc_range(i,x,y) for (auto i=x; i<=y; ++i)
+#define for_dec_range(i,x,y) for (auto i=x; i>=y; --i)
+
 #define fill0(x) memset(x, 0, sizeof(x))
 #define INT_INF 2E9L
+
+#define ntype int
+#define N_INF INT_INF
 
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
