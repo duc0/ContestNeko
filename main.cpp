cc313df3f1fd5fc575863e771737c63d91cfea91
duc <ducnm0@gmail.com>
2015-02-16 10:13:36 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -32,14 +32,79 @@ using namespace std;
 #define N_INF INT_INF
 
 void testGen() {
-  freopen("biginput1.txt", "w", stdout);
+  freopen("biginput2.txt", "w", stdout);
+  int n = 10000;
+  cout << n << endl;
+  repeat(n) {
+    cout << rand() % 1000003 - 590000 << " ";
+  }
   fclose(stdout);
 }
 
+#define MAXN 1000010
+
+int n, a[MAXN];
+
+int64 f[MAXN];
+
+void solve1() {
+  f[0] = 0;
+  for_inc_range(i, 1, n) {
+    int minx = a[i], maxx = a[i];
+    for_dec_range(j, i, 1) {
+      minx = min(minx, a[j]);
+      maxx = max(maxx, a[j]);
+      f[i] = max(f[i], f[j - 1] + maxx - minx);
+    }
+  }
+  cout << f[n] << endl;
+}
+
+void solve2() {
+  f[0] = 0;
+  for_inc_range(i, 1, n) {
+    f[i] = f[i - 1];
+    for_inc_range(j, 1, i) {
+      f[i] = max(f[i], f[j - 1] + a[i] - a[j]);
+      f[i] = max(f[i], f[j - 1] + a[j] - a[i]);
+    }
+  }
+  
+  cout << f[n] << endl;
+}
+
+set<int64> max1, max2;
+
+void solve3() {
+  f[0] = 0;
+  max1.insert(-a[1]);
+  max2.insert(a[1]);
+  for_inc_range(i, 1, n) {
+    f[i] = f[i - 1];
+    f[i] = max(f[i], a[i] + *(--max1.end()));
+    f[i] = max(f[i], -a[i] + *(--max2.end()));
+    if (i < n) {
+      max1.insert(f[i] - a[i + 1]);
+      max2.insert(f[i] + a[i + 1]);
+    }
+  }
+  
+  cout << f[n] << endl;
+}
+
+
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("biginput2.txt", "r", stdin);
   
+  scanf("%d", &n);
+  for_inc_range(i, 1, n) {
+    scanf("%d", &a[i]);
+  }
+  
+  //solve1();
+  //solve2();
+  solve3();
   
   return 0;
 }
