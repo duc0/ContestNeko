9e0c80a97d7daef479144e533e8a6945f90e4166
duc <ducnm0@gmail.com>
2015-02-15 20:55:08 -0800

    Add a wrong sol

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ using namespace std;
 #define for_dec_range(i,x,y) for (auto i=x; i>=y; --i)
 
 #define fill0(x) memset(x, 0, sizeof(x))
-#define INT_INF 2E9L
+#define INT_INF 1E9L
 
 #define ntype int
 #define N_INF INT_INF
@@ -36,10 +36,88 @@ void testGen() {
   fclose(stdout);
 }
 
+bool win(int h1, int a1, int d1, int h2, int a2, int d2) {
+  int f1 = max(0, a1 - d2);
+  int f2 = max(a2 - d1, 0);
+  if (f1 == 0) {
+    return false;
+  }
+  if (f2 == 0) {
+    return true;
+  }
+  int t1 = h1 / f2;
+  if (h1 % f2 > 0) t1++;
+  int t2 = h2 / f1;
+  if (h2 % f1 > 0) t2++;
+  return t1 > t2;
+}
+
+int h1, a1, d1, h2, a2, d2, ph, pa, pd;
+
+void tryBuy(int money, int &x, int &y, int &z, int &px, int &py, int &pz) {
+  x += money / px;
+  money %= px;
+  y += money / py;
+  money %= py;
+  z += money / pz;
+  money %= pz;
+}
+
+#define buy(x,y,z) tryBuy(money, x, y, z, p##x, p##y, p##z)
+
+bool ok(int oMoney) {
+  int money;
+  int h, a, d;
+  
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(h,a,d);
+  if (win(h, a, d, h2, a2, d2)) return true;
+  
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(h, d, a);
+  if (win(h, a, d, h2, a2, d2)) return true;
+
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(a, h, d);
+  if (win(h, a, d, h2, a2, d2)) return true;
+
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(a, d, h);
+  if (win(h, a, d, h2, a2, d2)) return true;
+
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(d, a, h);
+  if (win(h, a, d, h2, a2, d2)) return true;
+
+  money = oMoney; h = h1; a = a1; d = d1;
+  buy(d, h, a);
+  if (win(h, a, d, h2, a2, d2)) return true;
+  
+  return false;
+}
+
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("input1.txt", "r", stdin);
   
+  
+  cin >> h1 >> a1 >> d1;
+  cin >> h2 >> a2 >> d2;
+  cin >> ph >> pa >> pd;
+  
+  int left = 0, right = INT_INF, mid, ret = 0;
+  
+  while (left <= right) {
+    mid = (left + right) / 2;
+    if (ok(mid)) {
+      ret = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  
+  cout << ret << endl;
   
   return 0;
 }
