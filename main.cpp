44d3f2173d66c6ab2b3179a23ef7f229bec1e2e6
duc <ducnm0@gmail.com>
2015-02-17 14:10:52 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -36,10 +36,58 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAXN 2020
+
+int nRow, nCol;
+char board[MAXN][MAXN];
+
+int getAt(int r, int c) {
+  if (r < 0 || r >= nRow || c < 0 || c >= nCol) return 0;
+  return board[r][c] == '.';
+}
+
+int getDeg(int r, int c) {
+  return getAt(r - 1, c) + getAt(r, c - 1) + getAt(r + 1, c) + getAt(r, c + 1);
+}
+
+set<pair<int, pair<int, int>>> node;
+
 int main() {
   //testGen();
   freopen("input1.txt", "r", stdin);
   
+  scanf("%d%d", &nRow, &nCol);
+  for_inc(r, nRow) {
+    scanf("%s", board[r]);
+  }
+  
+  for_inc(r, nRow) for_inc(c, nCol) {
+    if (board[r][c] == '.') {
+      int deg = getDeg(r, c);
+      if (deg == 0) {
+        // No sol
+        cout << "Not unique";
+        return 0;
+      }
+      node.insert(make_pair(deg, make_pair(r, c)));
+    }
+  }
+  
+  while (node.size() > 0) {
+    auto p = node.begin();
+    if (p->first > 1) {
+      cout << "Not unique";
+      return 0;
+    }
+    int r = p->second.first;
+    int c = p->second.second;
+    if (getAt(r - 1, c) == 1) {
+      board[r-1][c] = '^';
+      board[r][c] = 'v';
+      update(r - 1, c);
+      update(r, c);
+    }
+  }
   
   return 0;
 }
