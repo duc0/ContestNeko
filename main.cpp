38c9c98e9d70f0b70a36eb9a1456845b9ed7f7f2
duc <ducnm0@gmail.com>
2015-02-16 23:58:00 -0800

    Add a TLE solution

--- main.cpp
+++ main.cpp
@@ -36,10 +36,135 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAXN 2020
+#define MAXK 2020
+
+int nRow, nCol, nCar;
+char board[MAXN][MAXN];
+pair<int, int> carPos[MAXK];
+int f[MAXN][MAXN];
+
+int ret[MAXK];
+
+set<int> carAt[MAXN];
+
+set<pair<int, int>> carIn;
+
+bool isGood(int side, int c) {
+  int minLeft = 1, maxRight = nCol;
+  auto nextCar = carIn.lower_bound(make_pair(c, 0));
+  if (nextCar != carIn.end()) {
+    if (nextCar->first != c) {
+      maxRight = nextCar->first - 1;
+    } else {
+      return false;
+    }
+  }
+  if (nextCar != carIn.begin()) {
+    nextCar--;
+    minLeft = nextCar->first + 1;
+  }
+  return minLeft <= maxRight && maxRight - minLeft + 1 >= side;
+}
+
+bool ok(int side, int r, int c) {
+  if (side == 1) {
+    return true;
+  }
+  
+  int top = max(1, r - side + 1), bottom = top + side - 1;
+  if (bottom > nRow || top > r) {
+    return false;
+  }
+  carIn.clear();
+  for_inc_range(row, top, bottom) {
+    for (auto &p : carAt[row]) {
+      carIn.insert(make_pair(p, row));
+    }
+  }
+  
+  while (1) {
+    if (isGood(side, c)) {
+      return true;
+    }
+    
+    for (auto &p: carAt[top]) {
+      carIn.erase(make_pair(p, top));
+    }
+    top++;
+    bottom++;
+    if (bottom > nRow || top > r) {
+      break;
+    }
+    for (auto &p: carAt[bottom]) {
+      carIn.insert(make_pair(p, bottom));
+    }
+    
+  }
+  return false;
+}
+
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("input4.txt", "r", stdin);
   
+  scanf("%d%d%d", &nRow, &nCol, &nCar);
+  for_inc(row, nRow) {
+    scanf("%s", board[row]);
+  }
+  
+  for_inc_range(i, 1, nCar) {
+    int r, c;
+    scanf("%d%d", &r, &c);
+    carPos[i] = make_pair(r, c);
+    board[r - 1][c - 1] = 'X';
+  }
+  
+  ret[nCar] = 0;
+  fill0(f);
+  for_inc_range(r, 1, nRow) {
+    for_inc_range(c, 1, nCol) {
+      char ch = board[r - 1][c - 1];
+      if (ch == '.') {
+        f[r][c] = min(f[r - 1][c], min(f[r][c - 1], f[r - 1][c - 1])) + 1;
+        ret[nCar] = max(ret[nCar], f[r][c]);
+      }
+    }
+  }
+  
+  for_inc_range(r, 1, nRow) {
+    for_inc_range(c, 1, nCol) {
+      char ch = board[r - 1][c - 1];
+      if (ch == 'X') {
+        carAt[r].insert(c);
+      }
+    }
+  }
+
+  for_dec_range(car, nCar - 1, 1) {
+    pair<int, int> pos = carPos[car + 1];
+    board[pos.first - 1][pos.second - 1] = '.';
+    
+    assert(carAt[pos.first].count(pos.second));
+    carAt[pos.first].erase(pos.second);
+    
+    int l = 1, r = min(nRow, nCol), mid = 0, best = 0;
+    while (l <= r) {
+      mid = (l + r) / 2;
+      if (ok(mid, pos.first, pos.second)) {
+        best = mid;
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    
+    ret[car] = max(ret[car + 1], best);
+  }
+  
+  for_inc_range(car, 1, nCar) {
+    cout << ret[car] << endl;
+  }
   
   return 0;
 }
