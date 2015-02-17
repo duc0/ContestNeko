cb97f584ccc2650176b4b2c651e6b463282c8620
duc <ducnm0@gmail.com>
2015-02-16 22:20:05 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -36,10 +36,58 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MOD 1000000007
+int MODP(int a) {
+  int r = a % MOD;
+  if (r < 0) r += MOD;
+  return r;
+}
+
+#define MAXN 5100
+
+int n, a, b, k;
+int f[MAXN][MAXN];
+int s[MAXN][MAXN];
+
+int getSum(int d, int i, int j) {
+  return MODP(s[d][j] - s[d][i - 1]);
+}
+
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("input3.txt", "r", stdin);
   
+  cin >> n >> a >> b >> k;
+  fill0(f);
+  fill0(s);
   
+  f[0][a] = 1;
+
+  s[0][0] = 0;
+  for_inc_range(i, 1, n) {
+    s[0][i] = MODP(s[0][i - 1] + f[0][i]);
+  }
+  
+  for_inc_range(d, 1, k) {
+    s[d][0] = 0;
+    for_inc_range(i, 1, n) {
+      if (i == b) continue;
+      if (i > b) {
+        f[d][i] = MODP(getSum(d - 1, (i + b) / 2 + 1, n) - f[d - 1][i]);
+      } else {
+        int r = (i + b) / 2;
+        if ((i + b) % 2 == 0) r--;
+        f[d][i] = MODP(getSum(d - 1, 1, r) - f[d - 1][i]);
+      }
+      
+      s[d][i] = MODP(s[d][i - 1] + f[d][i]);
+    }
+  }
+  
+  int ret = 0;
+  for_inc_range(i, 1, n) {
+    ret = MODP(ret + f[k][i]);
+  }
+  cout << ret << endl;
   return 0;
 }
