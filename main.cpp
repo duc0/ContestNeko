9829205bc9cb0eb8d635e0b8837b6a417dd99af2
duc <ducnm0@gmail.com>
2015-02-16 21:48:33 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -36,10 +36,59 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAXN 100100
+int a[MAXN];
+
+set<int> s;
+
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("input1.txt", "r", stdin);
   
+  int n, l, x, y;
+  cin >> n >> l >> x >> y;
+  for_inc_range(i, 1, n) {
+    cin >> a[i];
+    s.insert(a[i]);
+  }
+  
+  bool covx = false, covy = false;
+  for_inc_range(i, 1, n) {
+    if (!covx) covx = s.count(a[i] - x) || s.count(a[i] + x);
+    if (!covy) covy = s.count(a[i] - y) || s.count(a[i] + y);
+  }
+  
+  if (covx && covy) {
+    cout << 0 << endl;
+    return 0;
+  }
+  if (covx) {
+    cout << 1 << endl;
+    cout << y << endl;
+    return 0;
+  }
+  if (covy) {
+    cout << 1 << endl;
+    cout << x << endl;
+    return 0;
+  }
+  
+  for_inc_range(i, 1, n) {
+    int c;
+    for (int kind = 0; kind < 2; ++kind) {
+      c = kind == 0 ? (a[i] - x) : (x + a[i]);
+      if (0 <= c && c <= l) {
+        if (s.count(y + c) || s.count(c - y)) {
+          cout << 1 << endl;
+          cout << c << endl;
+          return 0;
+        }
+      }
+    }
+  }
+  
+  cout << 2 << endl;
+  cout << x << " " << y << endl;
   
   return 0;
 }
