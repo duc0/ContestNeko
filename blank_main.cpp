6ed7e767cb4885807622d19c74a891ee8891cf28
duc <ducnm0@gmail.com>
2015-02-20 20:25:37 -0800

    Add blank main file

---
+++ blank_main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+//#define NDEBUG
+#include <cassert>
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <cstdlib>
+#include <array>
+#include <type_traits>
+#include <queue>
+#include <stack>
+#include <functional>
+
+using namespace std;
+
+#define int64 long long
+#define repeat(x) for (auto repeat_var = 0; repeat_var < x; ++repeat_var)
+
+#define for_inc(i, x) for (auto i = 0; i < x; ++i)
+#define for_dec(i, x) for (auto i = x - 1; i >= 0; --i)
+#define for_inc_range(i, x, y) for (auto i = x; i <= y; ++i)
+#define for_dec_range(i, x, y) for (auto i = x; i >= y; --i)
+
+#define fill0(x) memset(x, 0, sizeof(x))
+#define INT_INF 2E9L
+
+void testGen() {
+  freopen("biginput1.txt", "w", stdout);
+  fclose(stdout);
+}
+
+int main() {
+  // freopen("input1.txt", "r", stdin);
+  return 0;
+}
---
+++ blank_main.h
@@ -0,0 +1,14 @@
+//
+//  blank_main.h
+//  CPPProjects
+//
+//  Created by Duc Ngo on 2/20/15.
+//  Copyright (c) 2015 PP. All rights reserved.
+//
+
+#ifndef __CPPProjects__blank_main__
+#define __CPPProjects__blank_main__
+
+#include <stdio.h>
+
+#endif /* defined(__CPPProjects__blank_main__) */
