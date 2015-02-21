4d4291e377a5ac5c3d5cdbe9da1e0d3cef319197
duc <ducnm0@gmail.com>
2015-02-20 17:47:18 -0800

    Reformat & better abstraction

--- main.cpp
+++ main.cpp
@@ -18,105 +18,134 @@
 using namespace std;
 
 #define int64 long long
-#define repeat(x) for(auto repeat_var=0;repeat_var<x;++repeat_var)
+#define repeat(x) for (auto repeat_var = 0; repeat_var < x; ++repeat_var)
 #define fill0(x) memset(x, 0, sizeof(x))
 #define INT_INF 2E9L
 #define INT64_INF 2E18L
 
-#define ntype int
-#define N_INF INT_INF
-
 void testGen() {
   freopen("biginput2.txt", "w", stdout);
   int n = 14;
-  cout << n << " " << n << " " << n * n << " " << n * n - 1 << endl;
+  cout << n << " " << n << " " << n *n << " " << n *n - 1 << endl;
   repeat(n) {
-    repeat (n) {
-      cout << ".";
-    }
+    repeat(n) { cout << "."; }
     cout << endl;
   }
-  repeat(2*n*n) {
-    cout << 1 << " " << 1 << " " << 1 << endl;
-  }
+  repeat(2 * n * n) { cout << 1 << " " << 1 << " " << 1 << endl; }
   fclose(stdout);
 }
 
-struct Network {
-  struct Edge {int u, v; ntype f, c;};
+template <class T> class Network {
+  struct Edge {
+    int u, v;
+    T f, c;
+  };
   vector<Edge> edgeList;
-  vector<vector<int> > adj;
-  void addEdge(int u, int v, ntype c) {
-    Edge e={u,v,0,c};
+  vector<vector<int>> adj;
+
+public:
+  vector<int> &getAdjacent(int u) { return adj[u]; }
+
+  Edge &getEdge(int i) { return edgeList[i]; }
+
+  void addEdge(int u, int v, T c) {
+    Edge e = {u, v, 0, c};
     adj[u].push_back((int)edgeList.size());
     adj[v].push_back((int)edgeList.size());
     edgeList.push_back(e);
   }
-  int n(){return (int)adj.size();}
+  size_t getSize() { return adj.size(); }
   void resetFlow() {
-    for (auto &pe : edgeList) pe.f=0;
+    for (auto &pe : edgeList)
+      pe.f = 0;
   }
   void init(int n) {
     edgeList.clear();
     adj.resize(n);
-    for (auto &au : adj) au.clear();
+    for (auto &au : adj)
+      au.clear();
   }
-  Network(int n){init(n);}
+  Network(int n) { init(n); }
 };
 
-class Dinic {
-  Network &g;
+template <class T, T INF> class Dinic {
+  Network<T> &g;
   int s, t;
-  vector<ntype> dist;
+  vector<T> dist;
   vector<bool> block;
   bool computeDist() {
     queue<int> q;
     q.push(t);
-    fill(dist.begin(),dist.end(),-1);
-    dist[t]=0;
+    fill(dist.begin(), dist.end(), -1);
+    dist[t] = 0;
     while (!q.empty()) {
-      int u=q.front();
+      int u = q.front();
       q.pop();
-      for (auto pi : g.adj[u]) {
-        Network::Edge &e=g.edgeList[pi];
-#define CONSIDER(v) {if (dist[v]==-1) {dist[v]=dist[u]+1; q.push(v); if (v==s) return true;}}
-        if (e.v==u && e.f<e.c) CONSIDER(e.u) else if (e.u==u && e.f>0) CONSIDER(e.v)
-#undef CONSIDER
-          }
-    }
-    return dist[s]!=-1;
-  }
-  ntype findAugmentingPath(int u, ntype delta) {
-    if (u==t) return delta;
-    ntype inc;
-    for (auto pi : g.adj[u]){
-      Network::Edge &e=g.edgeList[pi];
-#define CONSIDER(v,i,d) {if (!block[v] && dist[u]==dist[v]+1 && (inc=findAugmentingPath(v,min(delta,d)))) {e.f+=i*inc; return inc;}}
-      if (e.u==u && e.f<e.c) CONSIDER(e.v,1,e.c-e.f) else if (e.v==u && e.f>0) CONSIDER(e.u,-1,e.f)
-#undef CONSIDER
+      for (auto pi : g.getAdjacent(u)) {
+        auto &e = g.getEdge(pi);
+        int v = -1;
+        if (e.v == u && e.f < e.c)
+          v = e.u;
+        else if (e.u == u && e.f > 0)
+          v = e.v;
+        if (v != -1 && dist[v] == -1) {
+          dist[v] = dist[u] + 1;
+          q.push(v);
+          if (v == s)
+            return true;
         }
-    block[u]=true;
+      }
+    }
+    return dist[s] != -1;
+  }
+  T findAugmentingPath(int u, T delta) {
+    if (u == t)
+      return delta;
+    T inc;
+    for (auto pi : g.getAdjacent(u)) {
+      auto &e = g.getEdge(pi);
+      int v = -1, i = 0, d = 0;
+      if (e.u == u && e.f < e.c) {
+        v = e.v;
+        i = 1;
+        d = e.c - e.f;
+      } else if (e.v == u && e.f > 0) {
+        v = e.u;
+        i = -1;
+        d = e.f;
+      }
+      if (v != -1 && !block[v] && dist[u] == dist[v] + 1 &&
+          (inc = findAugmentingPath(v, min(delta, d)))) {
+        e.f += i * inc;
+        return inc;
+      }
+    }
+    block[u] = true;
     return 0;
   }
+
 public:
-  ntype totalFlow;
-  Dinic(Network &g, int s, int t):g(g),s(s),t(t){
+  T totalFlow;
+  Dinic(Network<T> &g, int s, int t) : g(g), s(s), t(t) {
     g.resetFlow();
-    totalFlow=0;
-    dist.resize(g.n());
-    block.resize(g.n());
+    totalFlow = 0;
+    dist.resize(g.getSize());
+    block.resize(g.getSize());
   }
-  ntype run () {
+  T run() {
     while (computeDist()) {
-		    fill(block.begin(), block.end(), false);
-		    while (ntype inc = findAugmentingPath(s, N_INF)) totalFlow += inc;
+      fill(block.begin(), block.end(), false);
+      while (T inc = findAugmentingPath(s, INF))
+        totalFlow += inc;
     }
     return totalFlow;
   }
 };
 
+// Sample problem: Rockethon 2015 - problem F
+
 #define MAXN 23
-#define MAXS MAXN*MAXN
+#define MAXS MAXN *MAXN
 
 #define MALE 0
 #define FEMALE 1
@@ -157,9 +186,9 @@ void addScay(int type, int r, int c, int t) {
 #define SQUARENODEOUT(x) (nMale + nFemale + nSquare + x)
 
 int main() {
-  //testGen();
+  // testGen();
   freopen("biginput2.txt", "r", stdin);
-  
+
   cin >> nRow >> nCol >> nMale >> nFemale;
   if ((nMale + nFemale) % 2 == 0) {
     cout << -1;
@@ -194,7 +223,7 @@ int main() {
   } else {
     nMale++;
   }
-  
+
   REPROW(r) REPCOL(c) REPROW(r2) REPCOL(c2) {
     dist[r][c][r2][c2] = INT_INF;
     if (r2 == r && c2 == c) {
@@ -205,16 +234,15 @@ int main() {
       }
     }
   }
-  
+
   REPROW(rk) REPCOL(ck) REPROW(r) REPCOL(c) REPROW(r2) REPCOL(c2) {
     int d1 = dist[r][c][rk][ck];
     int d2 = dist[rk][ck][r2][c2];
     if (d1 != INT_INF && d2 != INT_INF) {
-      dist[r][c][r2][c2] = min (dist[r][c][r2][c2], d1 + d2);
+      dist[r][c][r2][c2] = min(dist[r][c][r2][c2], d1 + d2);
     }
   }
-  
-  
+
   int64 maxTime = -INT64_INF;
   REPROW(r) REPCOL(c) {
     REPMALE(m) {
@@ -226,7 +254,7 @@ int main() {
         maxTime = max(maxTime, timeMale);
       }
     }
-    
+
     REPFEMALE(f) {
       best[FEMALE][f][r][c] = INT64_INF;
       int dFemale = dist[r][c][row[FEMALE][f]][col[FEMALE][f]];
@@ -237,45 +265,39 @@ int main() {
       }
     }
   }
-  
+
   REPROW(r) REPCOL(c) {
     if (board[r][c] == '.') {
       squareID[r][c] = nSquare;
       nSquare++;
     }
   }
-  
+
   int64 left = 0, right = maxTime, mid, ret = -1;
   while (left <= right) {
     mid = (left + right) / 2;
 
     int nNode = nMale + nFemale + 2 * nSquare + 2;
-    Network g(nNode);
+    Network<int> g(nNode);
     int source = nNode - 2;
     int sink = nNode - 1;
-    REPMALE(m) {
-      g.addEdge(source, MALENODE(m), 1);
-    }
-    REPFEMALE(f) {
-      g.addEdge(FEMALENODE(f), sink, 1);
-    }
-    REPSQUARE(x) {
-      g.addEdge(SQUARENODEIN(x), SQUARENODEOUT(x), 1);
-    }
-    
+    REPMALE(m) { g.addEdge(source, MALENODE(m), 1); }
+    REPFEMALE(f) { g.addEdge(FEMALENODE(f), sink, 1); }
+    REPSQUARE(x) { g.addEdge(SQUARENODEIN(x), SQUARENODEOUT(x), 1); }
+
     REPMALE(m) REPROW(r) REPCOL(c) {
       if (best[MALE][m][r][c] != INT64_INF && best[MALE][m][r][c] <= mid) {
         g.addEdge(MALENODE(m), SQUARENODEIN(squareID[r][c]), 1);
       }
     }
-    
+
     REPFEMALE(f) REPROW(r) REPCOL(c) {
       if (best[FEMALE][f][r][c] != INT64_INF && best[FEMALE][f][r][c] <= mid) {
         g.addEdge(SQUARENODEOUT(squareID[r][c]), FEMALENODE(f), 1);
       }
     }
-    
-    int bestMatch = Dinic(g, source, sink).run();
+
+    int bestMatch = Dinic<int, (int)INT_INF>(g, source, sink).run();
     if (bestMatch == nMale) {
       ret = mid;
       right = mid - 1;
@@ -283,9 +305,8 @@ int main() {
       left = mid + 1;
     }
   }
-  
+
   cout << ret << endl;
-  
-    
+
   return 0;
 }
