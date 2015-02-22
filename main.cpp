03f4e5aa0913c33633992d501aed1ce672c76ddf
duc <ducnm0@gmail.com>
2015-02-21 19:06:56 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cstring>
 
-#define NDEBUG
+//#define NDEBUG
 #include <cassert>
 
 #include <iostream>
@@ -14,6 +14,7 @@
 #include <array>
 #include <type_traits>
 #include <queue>
+#include <deque>
 
 using namespace std;
 
@@ -50,11 +51,25 @@ int getDeg(int r, int c) {
   return getAt(r - 1, c) + getAt(r, c - 1) + getAt(r + 1, c) + getAt(r, c + 1);
 }
 
-set<pair<int, pair<int, int>>> node;
+deque<pair<int ,int>> node;
+
+void check(int r, int c) {
+  if (getAt(r, c) == 1 && getDeg(r, c) == 1) {
+    node.push_back(make_pair(r, c));
+  }
+}
+
+void update(int r, int c) {
+  check(r, c);
+  check(r, c + 1);
+  check(r - 1, c);
+  check(r + 1, c);
+  check(r, c - 1);
+}
 
 int main() {
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  //freopen("input1.txt", "r", stdin);
   
   scanf("%d%d", &nRow, &nCol);
   for_inc(r, nRow) {
@@ -68,26 +83,57 @@ int main() {
         // No sol
         cout << "Not unique";
         return 0;
+      } else if (deg == 1) {
+        node.push_back(make_pair(r, c));
       }
-      node.insert(make_pair(deg, make_pair(r, c)));
     }
   }
   
-  while (node.size() > 0) {
-    auto p = node.begin();
-    if (p->first > 1) {
+  while (!node.empty()) {
+    auto p = node.front();
+    node.pop_front();
+    int r = p.first;
+    int c = p.second;
+    if (board[r][c] != '.') continue;
+    int deg = getDeg(r, c);
+    if (deg > 1) {
       cout << "Not unique";
       return 0;
     }
-    int r = p->second.first;
-    int c = p->second.second;
+    if (deg == 0) continue;
     if (getAt(r - 1, c) == 1) {
       board[r-1][c] = '^';
       board[r][c] = 'v';
-      update(r - 1, c);
       update(r, c);
+      update(r - 1, c);
+    } else if (getAt(r + 1, c) == 1) {
+      board[r][c] = '^';
+      board[r +1][c] = 'v';
+      update(r, c);
+      update(r + 1, c);
+    } else if (getAt(r, c - 1) == 1) {
+      board[r][c - 1] = '<';
+      board[r][c] = '>';
+      update(r, c);
+      update(r, c - 1);
+    } else if (getAt(r, c + 1) == 1){
+      board[r][c] = '<';
+      board[r][c + 1] = '>';
+      update(r, c);
+      update(r, c + 1);
+    } else {
+      assert(false);
     }
   }
+  
+  for_inc(r, nRow) for_inc(c, nCol) {
+    if (board[r][c] == '.') {
+      cout << "Not unique";
+      return 0;
+    }
+  }
+  
+  for_inc(r, nRow) cout << board[r] << endl;
   
   return 0;
 }
