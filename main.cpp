4eb2e7fc7f33479e07f72aead7b3b025fb759c0c
duc <ducnm0@gmail.com>
2015-02-28 08:47:56 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Paste me into the FileEdit configuration dialog
+
 #define SUBMIT
 
 #ifdef SUBMIT
@@ -25,7 +27,9 @@
 
 using namespace std;
 
-#define LOG(l, x) if (l <= LOGLEVEL) cout << x << endl
+#define LOG(l, x)                                                              \
+if (l <= LOGLEVEL)                                                           \
+cout << x << endl
 
 #define int64 long long
 #define repeat(x) for (auto repeat_var = 0; repeat_var < x; ++repeat_var)
@@ -41,18 +45,216 @@ using namespace std;
 #define MOD 1000000007
 int MODP(int64 x) {
   int r = x % MOD;
-  if (r < 0) r += MOD;
+  if (r < 0)
+    r += MOD;
   return r;
 }
 
-void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  fclose(stdout);
+class ColorfulChocolates {
+public:
+   int maximumSpread( string s, int m ) {
+     int n = (int) s.length();
+     int best = 0;
+     for_inc(i, n) {
+       vector<int> d;
+       int k = 1;
+       for_dec_range(j, i - 1, 0) {
+         if (s[j] == s[i]) {
+           d.push_back(i - k - j);
+           k++;
+         }
+       }
+       k = 1;
+       for_inc_range(j, i + 1, n - 1) {
+         if (s[j] == s[i]) {
+           d.push_back(j - (i + k));
+           k++;
+         }
+       }
+       sort(d.begin(), d.end());
+       
+       int cur = 1;
+       int remain = m;
+       if (i == 4) {
+         LOG(1, "HERE");
+       }
+       for (int x : d) {
+         if (x > remain) break;
+         remain -= x;
+         cur++;
+       }
+       best = max(best, cur);
+     }
+     return best;
+   }
+};
+
+// BEGIN CUT HERE
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace moj_harness {
+	using std::string;
+	using std::vector;
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			std::cerr << "No test cases run." << std::endl;
+		} else if (correct < total) {
+			std::cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << std::endl;
+		} else {
+			std::cerr << "All " << total << " tests passed!" << std::endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, std::clock_t elapsed) { 
+		std::cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			std::sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		std::cerr << verdict;
+		if (!info.empty()) {
+			std::cerr << " (";
+			for (size_t i=0; i<info.size(); ++i) {
+				if (i > 0) std::cerr << ", ";
+				std::cerr << info[i];
+			}
+			std::cerr << ")";
+		}
+		std::cerr << std::endl;
+		
+		if (verdict == "FAILED") {
+			std::cerr << "    Expected: " << expected << std::endl; 
+			std::cerr << "    Received: " << received << std::endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			string chocolates         = "ABCDCBC";
+			int maxSwaps              = 1;
+			int expected__            = 2;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			string chocolates         = "ABCDCBC";
+			int maxSwaps              = 2;
+			int expected__            = 3;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			string chocolates         = "ABBABABBA";
+			int maxSwaps              = 3;
+			int expected__            = 4;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			string chocolates         = "ABBABABBA";
+			int maxSwaps              = 4;
+			int expected__            = 5;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			string chocolates         = "QASOKZNHWNFODOQNHGQKGLIHTPJUVGKLHFZTGPDCEKSJYIWFOO";
+			int maxSwaps              = 77;
+			int expected__            = 5;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 5: {
+			string chocolates         = ;
+			int maxSwaps              = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 6: {
+			string chocolates         = ;
+			int maxSwaps              = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			string chocolates         = ;
+			int maxSwaps              = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = ColorfulChocolates().maximumSpread(chocolates, maxSwaps);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
 }
 
-int main() {
-#ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
-#endif
-  return 0;
+
+#include <cstdlib>
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(std::atoi(argv[i]));
+	}
 }
+// END CUT HERE
