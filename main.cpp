05ad1316188d5ce031b3baae74198430453ddf4e
duc <ducnm0@gmail.com>
2015-03-06 19:55:37 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define SUBMIT
+//#define SUBMIT
 
 #ifdef SUBMIT
 #define LOGLEVEL 0
@@ -50,10 +50,56 @@ void testGen() {
   fclose(stdout);
 }
 
+int rnd(double x) {
+  return (int)(x+0.5);
+}
+
+bool isSquare(int n) {
+  int p=rnd(sqrt(n));
+  return p*p==n;
+}
+
+
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input2.txt", "r", stdin);
 #endif
+  
+  int a, b;
+  cin >> a >> b;
+  for_inc_range(x1, 1, a - 1) {
+    int sqy1 = a * a - x1 * x1;
+    if (!isSquare(sqy1)) continue;
+    int y1 = (int)(sqrt(sqy1));
+    for_inc_range(x2, 1, b - 1) {
+      int sqy2 = b * b - x2 * x2;
+      if (!isSquare(sqy2)) continue;
+      int y2 = (int)(sqrt(sqy2));
+      
+      for_inc(sx1, 2) {
+        int ax1 = x1 * (2 * sx1 - 1);
+        for_inc(sy1, 2) {
+          int ay1 = y1 * (2 * sy1 - 1);
+          for_inc(sx2, 2) {
+            int ax2 = x2 * (2 * sx2 - 1);
+            for_inc(sy2, 2) {
+              int ay2 = y2 * (2 * sy2 - 1);
+              if ((ax1 * ax2 + ay1 * ay2 == 0) && (ax1 != ax2) && (ay1 != ay2)) {
+                cout << "YES" << endl;
+                cout << "0 0" << endl;
+                cout << ax1 << " " << ay1 << endl;
+                cout << ax2 << " " << ay2 << endl;
+                return 0;
+              }
+              
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  cout << "NO" << endl;
   return 0;
 }
