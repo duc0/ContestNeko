4e74dbe7f6ba30bb87a23b5ba75b3e674dd11319
duc <ducnm0@gmail.com>
2015-03-07 06:48:08 -0800

    Add sol

--- main.cpp
+++ main.cpp
@@ -50,10 +50,56 @@ void testGen() {
   fclose(stdout);
 }
 
+int n;
+int solve(const vector<int> &a, const vector<int> &b, const vector<int> &c) {
+  vector<int> f[4];
+  for_inc_range(k, 1, 3) {
+    f[k].resize(n + 1);
+  }
+  
+  f[1][0] = 0;
+  for_inc_range(i, 1, n) {
+    f[1][i] = a[i] + f[1][i - 1];
+  }
+  
+  for_inc_range(i, 2, n) {
+    f[2][i] = b[i] + f[1][i - 1];
+    if (i > 2) {
+      f[2][i] = min(f[2][i], b[i] + f[2][i - 1]);
+    }
+  }
+  
+  for_inc_range(i, 3, n) {
+    f[3][i] = c[i] + f[2][i - 1];
+    if (i > 3) {
+      f[3][i] = min(f[3][i], c[i] + f[3][i - 1]);
+    }
+  }
+  
+  return f[3][n];
+}
+
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input4.txt", "r", stdin);
 #endif
+  
+  cin >> n;
+  vector<int> a( n + 1), b( n + 1), c( n + 1);
+  for_inc_range(i, 1, n) cin >> a[i];
+  for_inc_range(i, 1, n) cin >> b[i];
+  for_inc_range(i, 1, n) cin >> c[i];
+  
+  int ret = INT_INF;
+  ret = min(ret, solve(a, b, c));
+  ret = min(ret, solve(a, c, b));
+  ret = min(ret, solve(b, a, c));
+  ret = min(ret, solve(b, c, a));
+  ret = min(ret, solve(c, a, b));
+  ret = min(ret, solve(c, b, a));
+  
+  cout << ret << endl;
+  
   return 0;
 }
