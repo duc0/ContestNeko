a8fe1d0dfd016b57c56ed582395bf8f41c368dd4
duc <ducnm0@gmail.com>
2015-03-17 10:53:12 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -46,15 +46,106 @@ int MODP(int64 x) {
   return r;
 }
 
+class BinarySearch {
+public:
+  template<class T> static T binarySearchMin(const T &minIndex, const T &maxIndex, const function<bool(T)> &predicate) {
+    T leftIndex = minIndex, rightIndex = maxIndex, midIndex, ret = maxIndex + 1;
+    while (leftIndex <= rightIndex) {
+      midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+      if (predicate(midIndex)) {
+        ret = midIndex;
+        rightIndex = midIndex - 1;
+      } else {
+        leftIndex = midIndex + 1;
+      }
+    }
+    return ret;
+  }
+  
+  template<class T> static T binarySearchMax(const T &minIndex, const T &maxIndex, const function<bool(T)> &predicate) {
+    T leftIndex = minIndex, rightIndex = maxIndex, midIndex, ret = minIndex - 1;
+    while (leftIndex <= rightIndex) {
+      midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+      if (predicate(midIndex)) {
+        ret = midIndex;
+        leftIndex = midIndex + 1;
+      } else {
+        rightIndex = midIndex - 1;
+      }
+    }
+    return ret;
+  }
+  
+  static double binarySearchMaxReal(double minRange, double maxRange, double epsilon, const function<bool(double)> &predicate) {
+    double l = minRange, r = maxRange, m, ret = maxRange + 1;
+    while (r - l > epsilon) {
+      m = l + (r - l) / 2;
+      if (predicate(m)) {
+        ret = m;
+        l = m;
+      } else {
+        r = m;
+      }
+    }
+    return ret;
+  }
+  
+  static double binarySearchMinReal(double minRange, double maxRange, double epsilon, const function<bool(double)> &predicate) {
+    double l = minRange, r = maxRange, m, ret = maxRange + 1;
+    while (r - l > epsilon) {
+      m = l + (r - l) / 2;
+      if (predicate(m)) {
+        l = m;
+        ret = m;
+      } else {
+        r = m;
+      }
+    }
+    return ret;
+  }
+  
+};
+
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
   fclose(stdout);
 }
+
+#define MAXN 200200
+pair<int, int> p[MAXN];
+int minVal[MAXN];
 
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  int n;
+  cin >> n;
+  
+  for_inc_range(i, 1, n) {
+    int x, w;
+    cin >> x >> w;
+    p[i] = make_pair(x, w);
+  }
+  
+  sort(p + 1, p + 1 + n);
+  
+  int longest = 1;
+  minVal[1] = p[1].first + p[1].second;
+  
+  longest = 1;
+  for_inc_range(i, 2, n) {
+    int k = BinarySearch::binarySearchMax<int>(1, longest, [&](int l) {return minVal[l] <= p[i].first - p[i].second;});
+    if (k == longest) {
+      longest++;
+      minVal[longest] = p[i].first + p[i].second;
+    } else {
+      minVal[k + 1] = min(minVal[k + 1], p[i].first + p[i].second);
+    }
+  }
+
+  cout << longest << endl;
   return 0;
 }
