c6a45b6890709fc1872c213386fbb29e8be9a872
duc <ducnm0@gmail.com>
2015-03-19 22:18:39 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -54,7 +54,58 @@ void testGen() {
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input2.txt", "r", stdin);
 #endif
+  string s;
+  int n;
+  cin >> n;
+  cin >> s;
+  vector<vector<int64>>f(n + 1, vector<int64>(n + 1));
+  vector<int64> pow(n + 1);
+  pow[0] = 1;
+  for_inc_range(i, 1, n) {
+    pow[i] = pow[i - 1] * 10;
+  }
+  
+  for_inc_range(i, 0, n) {
+    for_inc_range(j, 0, n) {
+      int pos = i + j;
+      
+      if (pos == 0) {
+        f[i][j] = 0;
+      } else {
+        
+        int x = s[pos - 1] - '0';
+        
+        f[i][j] = 0;
+        if (i > 0) {
+          f[i][j] = f[i - 1][j] + pow[n - i] * x;
+        }
+        if (j > 0) {
+          f[i][j] = max(f[i][j], f[i][j - 1] + pow[n - j] * x);
+        }
+      }
+    }
+  }
+  LOG(1, f[n][n]);
+
+  int curI = n;
+  int curJ = n;
+  string ans = s;
+  while (curI > 0 || curJ > 0) {
+    int pos = curI + curJ;
+
+    int x = s[pos - 1] - '0';
+    
+    if (curI > 0 && (f[curI][curJ] == (f[curI - 1][curJ] + pow[n - curI] * x))) {
+      ans[pos - 1] = 'M';
+      curI--;
+    } else if (curJ > 0 && (f[curI][curJ] == (f[curI][curJ - 1] + pow[n - curJ] * x))) {
+      ans[pos - 1] = 'H';
+      curJ--;
+    }
+  }
+  
+  cout << ans << endl;
   return 0;
 }
