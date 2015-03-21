588d0a668393093092eb618f4985a628e658060a
duc <ducnm0@gmail.com>
2015-03-21 10:31:42 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -54,7 +54,86 @@ void testGen() {
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input3.txt", "r", stdin);
 #endif
+  
+  int n;
+  cin >> n;
+  
+  vector<pair<int, int>> rect( n+1);
+  
+  set<int> allS;
+  
+  for_inc_range(i, 1, n) {
+    int w, h;
+    cin >> w >> h;
+    rect[i] = make_pair(w, h);
+    
+    allS.insert(w);
+    allS.insert(h);
+  }
+  
+  vector<int> all(allS.begin(), allS.end());
+  
+  int best = INT_INF;
+  
+  for (auto &finalH: all) {
+    bool bad = false;
+    for_inc_range(i, 1, n) {
+      if (min(rect[i].first, rect[i].second) > finalH) {
+        bad = true;
+        break;
+      }
+    }
+    if (bad) {
+      continue;
+    }
+    
+    vector<bool> turn(n + 1);
+    int have = n / 2;
+    
+    vector<pair<int, int>> diff;
+    
+    for_inc_range(i, 1, n) {
+      if (rect[i].second > finalH) {
+        if (have == 0) {
+          bad = true;
+          break;
+        } else {
+          have--;
+          turn[i] = true;
+        }
+      } else if (rect[i].first <= finalH) {
+        diff.push_back(make_pair(rect[i].second - rect[i].first, i));
+      }
+      
+    }
+    
+    if (bad) {
+      continue;
+    }
+    
+    sort(diff.begin(), diff.end());
+    
+    for (auto &d: diff) {
+      if (have == 0) break;
+      if (d.first >= 0) break;
+      turn[d.second] = true;
+      have--;
+    }
+    
+    int sumw = 0;
+    for_inc_range(i, 1, n) {
+      if (turn[i]) sumw += rect[i].second; else sumw += rect[i].first;
+    }
+    
+    int ans = sumw * finalH;
+    best = min(best, ans);
+    
+    LOG(1, "FinalH: " << finalH << " " << sumw << " " << ans);
+  }
+  
+  cout << best << endl;
+  
   return 0;
 }
