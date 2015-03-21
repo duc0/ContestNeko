f61c80d4c087e656d037255d8d636fefe3a2a71f
duc <ducnm0@gmail.com>
2015-03-21 08:55:35 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -54,7 +54,39 @@ void testGen() {
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input2.txt", "r", stdin);
 #endif
+  
+  vector<pair<pair<int, int>, int>> rect;
+  
+  int n;
+  
+  int sumw = 0;
+  cin >> n;
+  for_inc(i, n) {
+    int ww, hh;
+    cin >> ww >> hh;
+    rect.push_back(make_pair(make_pair(hh, ww), i));
+    sumw += ww;
+  }
+  sort(rect.begin(), rect.end());
+  int maxH = rect[n - 1].first.first;
+  int maxH2 = rect[n - 2].first.first;
+  
+  vector<int> ans(n);
+  for_inc(i, n) {
+    int ww = sumw - rect[i].first.second;
+    int hh = maxH;
+    if (i == n - 1) {
+      hh = maxH2;
+    }
+    ans[rect[i].second] = ww * hh;
+  }
+  
+  for_inc(i, n) {
+    cout << ans[i] << " ";
+  }
+  
+  
   return 0;
 }
