834289e3a5ed0b7664a4f3074833a803de9dbbe8
duc <ducnm0@gmail.com>
2015-03-22 11:50:53 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -51,10 +51,67 @@ void testGen() {
   fclose(stdout);
 }
 
+// Greedy
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input3.txt", "r", stdin);
 #endif
+  
+  int n, m, t;
+  scanf("%d%d%d", &n, &m, &t);
+  
+  vector<int> color( n  + 1);
+  
+  vector<int> a(n + 1);
+  
+  int nColor = 0;
+  
+  int curL = 1;
+  
+  bool good = false;
+  map<int, int> dColor;
+  
+  for_inc_range(i, 1, n) {
+    int hour, min, sec;
+    scanf("%d:%d:%d", &hour, &min, &sec);
+    
+    //LOG(1, hour << " " << min << " " << sec);
+    
+    a[i] = hour * 3600 + min * 60 + sec;
+
+    LOG(1, "Time: " << a[i]);
+    
+    while (curL < i && a[curL] + t <= a[i]) {
+      dColor[color[curL]]--;
+      if (dColor[color[curL]] == 0) {
+        dColor.erase(color[curL]);
+      }
+      ++curL;
+    }
+    
+    if (dColor.size() >= m) {
+      color[i] = color[i - 1];
+      dColor[color[i]]++;
+      good = true;
+    } else {
+      if (i - curL + 1 == m) {
+        good = true;
+      }
+      nColor++;
+      color[i] = nColor;
+      dColor[color[i]]++;
+    }
+  }
+  
+  
+  if (good) {
+    cout << nColor << endl;
+    for_inc_range(i, 1, n) {
+      cout << color[i] << endl;
+    }
+  } else {
+    cout << "No solution" << endl;
+  }
   return 0;
 }
