877dff21a82014b1eef11a831430fdf469acfbcc
duc <ducnm0@gmail.com>
2015-03-23 21:00:27 -0700

    Add sol

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ using namespace std;
 #define for_dec_range(i, x, y) for (auto i = x; i >= y; --i)
 
 #define fill0(x) memset(x, 0, sizeof(x))
-#define INT_INF ((int)2E9L)
+#define INT_INF ((int)1E9L)
 #define INT64_INF ((int64)1E18L)
 #define MOD 1000000007
 int MODP(int64 x) {
@@ -54,7 +54,73 @@ void testGen() {
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("input3.txt", "r", stdin);
 #endif
+  
+  int n, m;
+  cin >> n >> m;
+  
+  vector<int> add(n + 1);
+  
+  vector<int> upper(n + 1);
+  for_inc_range(i, 1, n) {
+    upper[i] = INT_INF;
+  }
+
+  vector<int> qt(m + 1), ql(m + 1), qr(m + 1), qv(m + 1);
+  
+  for_inc_range(i, 1, m) {
+    cin >> qt[i] >> ql[i] >> qr[i] >> qv[i];
+  }
+  for_inc_range(i, 1, m) {
+    int t, l , r, v;
+    t = qt[i];
+    l = ql[i];
+    r = qr[i];
+    v = qv[i];
+    if (t == 1) {
+      for_inc_range(i, l, r) {
+        add[i] += v;
+      }
+    } else {
+      for_inc_range(i, l, r) {
+        upper[i] = min(upper[i], v - add[i]);
+      }
+    }
+  }
+  
+  vector<int> ans(n + 1);
+  for_inc_range(i, 1, n) {
+    ans[i] = upper[i];
+  }
+  vector<int> a = ans;
+  
+  for_inc_range(i, 1, m) {
+    int t, l , r, v;
+    t = qt[i];
+    l = ql[i];
+    r = qr[i];
+    v = qv[i];
+    if (t == 1) {
+      for_inc_range(i, l, r) {
+        a[i] += v;
+      }
+    } else {
+      int best = a[l];
+      for_inc_range(i, l, r) {
+        best = max(best, a[i]);
+      }
+      if (best != v) {
+        cout << "NO" << endl;
+        return 0;
+      }
+    }
+  }
+  
+  
+  cout << "YES" << endl;
+  for_inc_range(i, 1, n) {
+    cout << ans[i] << " ";
+  }
   return 0;
 }
