d799286ef831d15ec9e4a6dbee319912e4226cdb
duc <ducnm0@gmail.com>
2015-04-01 21:46:20 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -53,6 +53,69 @@ int MODP(int64 x) {
   return r;
 }
 
+// 0-based
+template<size_t NCOL> class GF2Matrix {
+  vector<bitset<NCOL>> row;
+  int nRow;
+  vector<int> order;
+  vector<size_t> length;
+  bool isLock;
+  
+public:
+  void init(int nRow) {
+    this->nRow = nRow;
+    row.resize(nRow);
+    order.resize(nRow);
+    for_inc(r, nRow) {
+      order[r] = r;
+    }
+    length.resize(nRow);
+    isLock = false;
+  }
+  
+  void setRow(int r, const bitset<NCOL> &rV) {
+    row[r] = rV;
+  }
+  
+  void lock() {
+    isLock = true;
+    for_inc(r, nRow) {
+      for_dec(c, NCOL) {
+        if (row[r].test(c)) {
+          length[r] = c;
+          break;
+        }
+      }
+    }
+  }
+  
+  // O(NROW log NROW + NROW * NCOL)
+  void gaussianElimination() {
+    assert(isLock);
+    sort(order.begin(), order.end(), [&](int r1, int r2) {return length[r1] > length[r2];});
+    for (int c = NCOL - 1, r = 0; c >=0; c--) {
+      int i = r;
+      while (i < nRow && !getRow(i).test(c)) {
+        ++i;
+      }
+      if (i >= nRow) {
+        continue;
+      }
+      swap(order[r], order[i]);
+      for_inc_range(j, r + 1, nRow - 1) {
+        if (getRow(j).test(c)) {
+          row[order[j]] ^= getRow(r);
+        }
+      }
+      r++;
+    }
+  }
+  
+  inline const bitset<NCOL> & getRow(int r) const {
+    return row[order[r]];
+  }
+};
+
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
   fclose(stdout);
@@ -63,5 +126,27 @@ int main() {
 #ifndef SUBMIT
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  int n;
+  cin >> n;
+  GF2Matrix<64> a;
+  a.init(n);
+  for_inc(r, n) {
+    int64 x;
+    cin >> x;
+    a.setRow(r, x);
+  }
+  
+  a.lock();
+  a.gaussianElimination();
+  
+  int64 ret = 0;
+  for_inc(r, n) {
+    int64 val = a.getRow(r).to_ullong();
+    if ((val ^ ret) > ret) {
+      ret ^= val;
+    }
+  }
+  cout << ret << endl;
   return 0;
 }
