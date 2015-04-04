ccc7fb5e2ee30507c10d7e065d30a06a742b4f81
duc <ducnm0@gmail.com>
2015-04-04 14:27:52 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -58,10 +58,87 @@ void testGen() {
   fclose(stdout);
 }
 
+int n, q;
+vector<int> a;
+
+int getA(int i) {
+  if (i > n) return a[i - n];
+  return a[i];
+}
+
+int solve(int64 cap) {
+  
+  vector<int> maxR(n + 1);
+  
+  int64 sum = 0;
+  int j = 0;
+  while (j + 1 <= n && sum + a[j + 1] <= cap) {
+    sum += a[j + 1];
+    ++j;
+  }
+  maxR[1] = j;
+  
+  for_inc_range(i, 2, n) {
+    sum -= a[i - 1];
+    
+    while (j + 1 < i + n - 1 && sum + getA(j + 1) <= cap) {
+      sum += getA(j + 1);
+      j++;
+    }
+    
+    maxR[i] = j;
+  }
+  
+  int minGroup = maxR[1], group = 1;
+  for_inc_range(i, 1, n) {
+    if (maxR[i] - i + 1 < minGroup) {
+      minGroup = maxR[i] - i + 1;
+      group = i;
+    }
+  }
+  
+  int ans = n;
+  for_inc_range(s, group, maxR[group] + 1) {
+    int start = s;
+    if (start > n) start -= n;
+    
+    int cand = 0;
+    int u = start;
+    
+    int total = 0;
+    while (true) {
+      cand++;
+      total += (maxR[u] - u + 1);
+      if (total >= n) {
+        break;
+      }
+      u = maxR[u] + 1;
+      if (u > n) {
+        u -= n;
+      }
+    }
+    ans = min(ans, cand);
+  }
+  return ans;
+}
+
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  cin >> n >> q;
+  
+  a.resize(n  + 1);
+  for_inc_range(i, 1, n) cin >> a[i];
+  
+  repeat(q) {
+    int64 cap;
+    cin >> cap;
+    cout << solve(cap) << endl;
+  }
+  
+  
   return 0;
 }
