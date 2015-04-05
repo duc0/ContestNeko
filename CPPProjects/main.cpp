2c74a5740916cf8d5244b0a2bcab5a0b3ec133a9
duc <ducnm0@gmail.com>
2015-04-04 19:27:05 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -53,15 +53,108 @@ int MODP(int64 x) {
   return r;
 }
 
+template <class T> class RangeQuery {
+  size_t n, k;
+  vector<vector<T>> a;
+  function<T(T, T)> combine;
+  
+public:
+  RangeQuery() {}
+  
+  template <class Iterator>
+  RangeQuery(Iterator begin, Iterator end) {
+    init(begin, end);
+  }
+  
+  template <class Iterator>
+  RangeQuery(Iterator begin, Iterator end, const function<T(T, T)> &combine) {
+    init(begin, end, combine);
+  }
+  
+  // The default combine function is min (Range Minimum Query).
+  template <class Iterator>
+  void init(Iterator begin, Iterator end) {
+    init(begin, end, [](const T &a, const T &b) { return min(a, b); });
+  }
+  
+  template <class Iterator>
+  void init(Iterator begin, Iterator end, const function<T(T, T)> &combine) {
+    this->combine = combine;
+    n = end - begin;
+    k = -1;
+    size_t s = n;
+    while (s > 0) {
+      s >>= 1;
+      ++k;
+    }
+    a.resize(k + 1);
+    for (int i = 0; i <= k; ++i) {
+      a[i].resize(n + 1 - (1 << i));
+    }
+    auto it = begin;
+    for (int i = 0; i < n; ++i) {
+      a[0][i] = *it;
+      ++it;
+    }
+    for (int t = 1; t <= k; ++t) {
+      for (int i = 0; i <= n - (1 << t); ++i) {
+        a[t][i] = combine(a[t - 1][i], a[t - 1][i + (1 << (t - 1))]);
+      }
+    }
+  }
+  
+  T query(int i, int j) {
+    int t = 31 - __builtin_clz(j - i + 1);
+    int m = j + 1 - (1 << t);
+    return min(a[t][i], a[t][m]);
+  }
+};
+
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
+  int n = 100000;
+  cout << n << endl;
+  repeat(n) {
+    cout << rand() % 1000000000 + 1 << " ";
+  }
+  cout << endl;
+  int m = (int) 1E8;
+  cout << m << endl;
+  cout << 0 << " " << 1 << endl;
   fclose(stdout);
 }
+
+int n, m;
+vector<int> a;
 
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
-  freopen("input1.txt", "r", stdin);
+  freopen("biginput1.txt", "r", stdin);
 #endif
+  
+  scanf("%d", &n);
+  a.resize(n + 1);
+  for_inc_range(i, 1, n) {
+    scanf("%d", &a[i]);
+    a[i] = -a[i];
+  }
+  
+  RangeQuery<int> rmq(a.begin(), a.end());
+  
+  int64 ret = 0;
+  int x, y;
+  scanf("%d", &m);
+  scanf("%d%d", &x, &y);
+  repeat(m) {
+    ret += -rmq.query(min(x, y) + 1, max(x, y) + 1);
+    
+    x += 7;
+    while (x >= (n - 1)) x -= (n - 1);
+    y += 11;
+    while (y >= n) y -= n;
+  }
+  
+  cout << ret << endl;
   return 0;
 }
