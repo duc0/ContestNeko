7a54c480787be4f2b65c8ab09fff19fa7f097d98
duc <ducnm0@gmail.com>
2015-04-04 18:38:56 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -53,9 +53,81 @@ int MODP(int64 x) {
   return r;
 }
 
+
+// Note: 0-base index
+template<class T> class ZFunction {
+  int n;
+  vector<int> z; // z[i] = max{l, s[i..i + l - 1] is a prefix of s, or 0 if s[i] != s[0]}
+public:
+  // O(n)
+  template<class Iterator> ZFunction(Iterator begin, Iterator end) {
+    n = (int) (end - begin);
+    z.resize(n);
+    z[0] = n;
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+      if (i <= r) {
+        z[i] = min(r - i + 1, z[i - l]);
+      }
+      while (i + z[i] < n && *(begin + z[i]) == *(begin + i + z[i])) {
+        ++z[i];
+      }
+      if (i + z[i] - 1 > r) {
+        l = i;
+        r = i + z[i] - 1;
+      }
+    }
+  }
+  
+  int get(int i) const {
+    assert(0 <= i && i < n);
+    return z[i];
+  }
+};
+
+// 0 based index
+template<class T> class StringPeriod {
+  int n;
+  vector<int> period; // period[i] = min{l, s[0..(l - 1)] is a base string of s[0..i]}
+public:
+  // O(nlogn)
+  template<class Iterator> StringPeriod(Iterator begin, Iterator end) {
+    n = (int) (end - begin);
+    period.resize(n);
+    ZFunction<char> z(begin, end);
+    
+    for_dec_range(len, n, 1) {
+      for (int end = len - 1; end < n; end += len) {
+        int start = end - len + 1;
+        if (z.get(start) < len) break;
+        period[end] = len;
+      }
+    }
+  }
+  
+  int get(int i) const {
+    assert(0 <= i && i < n);
+    return period[i];
+  }
+};
+
+
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
   fclose(stdout);
+}
+
+int nTest;
+int n;
+string s;
+
+void solve() {
+  StringPeriod<char> p(s.begin(), s.end());
+  
+  for_inc(i, n) {
+    if (p.get(i) != (i + 1)) {
+      cout << i + 1 << " " << (i + 1) / p.get(i) << endl;
+    }
+  }
 }
 
 int main() {
@@ -63,5 +135,14 @@ int main() {
 #ifndef SUBMIT
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  cin >> nTest;
+  for_inc_range(test, 1, nTest) {
+    cin >> n;
+    cin >> s;
+    cout << "Test case #" << test << endl;
+    solve();
+    cout << endl;
+  }
   return 0;
 }
