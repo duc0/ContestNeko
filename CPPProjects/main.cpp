3c78c0ca33b384631c6cc6db852713b2f7f37786
duc <ducnm0@gmail.com>
2015-04-09 09:26:13 -0700

    Add TLE sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -1,13 +1,9 @@
-//#define SUBMIT
-
-#define C11
-
-#ifdef SUBMIT
 #define LOGLEVEL 0
 #define NDEBUG
-#else
+
+// BEGIN CUT HERE
 #define LOGLEVEL 1
-#endif
+// END CUT HERE
 
 #include <cstdio>
 #include <algorithm>
@@ -19,22 +15,18 @@
 #include <set>
 #include <cmath>
 #include <cstdlib>
+#include <array>
+#include <type_traits>
 #include <queue>
 #include <stack>
 #include <functional>
 #include <sstream>
+#include <unordered_set>
+#include <unordered_map>
 #include <deque>
 #include <climits>
 #include <cfloat>
-#include <bitset>
-
-#ifdef C11
-#include <array>
-#include <type_traits>
 #include <random>
-#include <unordered_set>
-#include <unordered_map>
-#endif
 
 using namespace std;
 
@@ -52,22 +44,237 @@ using namespace std;
 #define INT_INF ((int)2E9L)
 #define INT64_INF ((int64)1E18L)
 #define MOD 1000000007
-int MODP(int64 x) {
-  int r = x % MOD;
-  if (r < 0) r += MOD;
-  return r;
+
+#define MAXQ 6
+#define MAXD 5050
+bool ques[MAXQ][MAXD];
+int cnt[2][70000];
+
+class Nine {
+  int nQues;
+  vector<int> getRem(int s) {
+    vector<int> rem(nQues);
+    for_inc(i, nQues) {
+      rem[i] = s % 9;
+      s /= 9;
+    }
+    return rem;
+  }
+  
+  int toS(const vector<int> &rem) {
+    int s = 0;
+    for_dec(i, nQues) {
+      s = s * 9 + rem[i];
+    }
+    return s;
+  }
+  
+public:
+  int count( int nQues, vector <int> d ) {
+    this->nQues = nQues;
+    int nDigit = (int) d.size();
+    fill0(ques);
+    
+    for_inc(q, nQues) {
+      for_inc(i, nDigit) {
+        if (d[i] & (1 << q)) {
+          ques[q][i] = true;
+        }
+      }
+    }
+    
+    int totalS = 1;
+    for_inc(k, nQues) totalS *= 9;
+    
+    fill0(cnt);
+    int cur = 0, next = 1;
+    cnt[cur][0] = 1;
+    for_inc_range(i, 1, nDigit) {
+      fill0(cnt[next]);
+      for_inc(s, totalS) {
+        for_inc(d, 10) {
+          vector<int> rem = getRem(s);
+          for_inc(k, nQues) if (ques[k][i - 1]) {
+            rem[k] = (rem[k] + 9 - d) % 9;
+          }
+          int s2 = toS(rem);
+          cnt[next][s] = (cnt[next][s] + cnt[cur][s2]) % MOD;
+        }
+      }
+      cur = 1 - cur;
+      next = 1 - next;
+    }
+    
+    return cnt[cur][0];
+  }
+};
+
+// BEGIN CUT HERE
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace moj_harness {
+	using std::string;
+	using std::vector;
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			std::cerr << "No test cases run." << std::endl;
+		} else if (correct < total) {
+			std::cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << std::endl;
+		} else {
+			std::cerr << "All " << total << " tests passed!" << std::endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, std::clock_t elapsed) { 
+		std::cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			std::sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		std::cerr << verdict;
+		if (!info.empty()) {
+			std::cerr << " (";
+			for (size_t i=0; i<info.size(); ++i) {
+				if (i > 0) std::cerr << ", ";
+				std::cerr << info[i];
+			}
+			std::cerr << ")";
+		}
+		std::cerr << std::endl;
+		
+		if (verdict == "FAILED") {
+			std::cerr << "    Expected: " << expected << std::endl; 
+			std::cerr << "    Received: " << received << std::endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			int N                     = 2;
+			int d[]                   = {1,2};
+			int expected__            = 4;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			int N                     = 2;
+			int d[]                   = {1,2,3};
+			int expected__            = 16;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			int N                     = 1;
+			int d[]                   = {0,0,1};
+			int expected__            = 200;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			int N                     = 5;
+			int d[]                   = {1,3,5,8,24,22,25,21,30,2,4,0,6,7,9,11,14,13,12,15,18,17,16,19,26,29,31,28,27,10,20,23};
+			int expected__            = 450877328;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			int N                     = 5;
+			int d[]                   = {31,31,31,31,31};
+			int expected__            = 11112;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 5: {
+			int N                     = ;
+			int d[]                   = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 6: {
+			int N                     = ;
+			int d[]                   = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			int N                     = ;
+			int d[]                   = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Nine().count(N, vector <int>(d, d + (sizeof d / sizeof d[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
 }
 
-void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  fclose(stdout);
-}
 
-int main() {
-  ios::sync_with_stdio(false);
-#ifndef SUBMIT
-  //testGen();
-  freopen("input1.txt", "r", stdin);
-#endif
-  return 0;
+#include <cstdlib>
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(std::atoi(argv[i]));
+	}
 }
+// END CUT HERE
