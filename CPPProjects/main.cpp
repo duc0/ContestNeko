565225cc5498a24deef26c213540f9896f2e3a40
duc <ducnm0@gmail.com>
2015-04-09 21:00:11 -0700

    Max matching sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -146,93 +146,217 @@ public:
   }
 };
 
-class BipartiteGraph {
-  friend class BipartiteMatching;
-
+class UndirectedGraph {
+  friend class UndirectedGraphEdgeMarker;
+  
   vector<vector<int>> adj;
-  int nLeft, nRight;
-
-  void reset(int nLeft, int nRight) {
-    this->nLeft = nLeft;
-    this->nRight = nRight;
-    adj.resize(nLeft + 1);
-    for_inc_range(u, 1, nLeft) adj[u].clear();
-  }
-
-public:
-  BipartiteGraph(int nLeft, int nRight) {
-    reset(nLeft, nRight);
-  }
-
-  void addEdge(int x, int y) {
-    assert(1 <= x && x <= nLeft);
-    assert(1 <= y && y <= nRight);
-    adj[x].push_back(y);
-  }
-};
-
-// O(V^3)
-struct BipartiteMatching {
-  vector<int> pre;
-  vector<int> mx, my;
-  int nMatch;
-  const BipartiteGraph &g;
-
-  bool match(int x) {
-    if (x == -1)
-      return true;
-    for (int y : g.adj[x]) {
-      if (pre[y] != -1)
-        continue;
-      pre[y] = x;
-      if (match(my[y])) {
-        my[y] = x;
-        mx[x] = y;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  void greedyMatch() {
-    for_inc_range(x, 1, g.nLeft) {
-      if (mx[x] == -1) {
-        for (int y : g.adj[x]) {
-          if (my[y] == -1) {
-            mx[x] = y;
-            my[y] = x;
-            ++nMatch;
-            break;
-          }
+  // indexMap[u][i] = the index of the same edge in adj[v]
+  vector<vector<int>> indexMap;
+  vector<int> comp;
+  vector<int> color;
+  vector<int> deg;
+  bool ccBuilt = false;
+  int nEdge = 0;
+  int n, nComp;
+  bool _hasOddCycle = false;
+  
+  void dfs(int u, int c) {
+    comp[u] = nComp;
+    color[u] = c;
+    for (auto &v: adj[u]) {
+      if (comp[v] == -1) {
+        dfs(v, 1 - c);
+      } else {
+        if (color[v] != 1 - c) {
+          _hasOddCycle = true;
         }
       }
     }
   }
+  
+public:
+  void init(int n) {
+    this->n = n;
+    adj.resize(n + 1);
+    indexMap.resize(n + 1);
+    deg.resize(n + 1);
+    for_inc_range(u, 1, n) {
+      adj[u].clear();
+      indexMap[u].clear();
+    }
+    nEdge = 0;
+    ccBuilt = false;
+  }
+  
+  const vector<int> &getAdjacent(int u) const {
+    return adj[u];
+  }
+  
+  void addEdge(int u, int v) {
+    assert(1 <= u && u <= n);
+    assert(1 <= v && v <= n);
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    indexMap[u].push_back((int)adj[v].size() - 1);
+    indexMap[v].push_back((int)adj[u].size() - 1);
+    nEdge++;
+    deg[u]++;
+    deg[v]++;
+    ccBuilt = false;
+  }
+  
+  int getDeg(int u) const {
+    return deg[u];
+  }
+  
+  int getSize() const {
+    return n;
+  }
+  
+  int getNumEdges() const {
+    return nEdge;
+  }
+  
+  // Find connected components and detect odd cycle
+  void buildCC() {
+    ccBuilt = true;
+    comp.resize(n + 1);
+    fill(comp.begin(), comp.end(), -1);
+    color.resize(n + 1);
+    nComp = 0;
+    _hasOddCycle = false;
+    for_inc_range(u, 1, n) {
+      if (comp[u] == -1) {
+        nComp++;
+        dfs(u, 0);
+      }
+    }
+  }
+  
+  bool hasOddCycle() {
+    assert(ccBuilt);
+    return _hasOddCycle;
+  }
+  
+  int getComponent(int u) {
+    assert(ccBuilt);
+    return comp[u];
+  }
+  
+  int getNumberOfComponents() {
+    assert(ccBuilt);
+    return nComp;
+  }
+};
+
+class MaximumMatching {
+  const UndirectedGraph &g;
+  vector<int> match;
+  vector<int> p;
+  int nMatch;
+  int n;
+  
+  int lca(const vector<int> &base, int a, int b) {
+    vector<bool> used(match.size());
+    while (true) {
+      a = base[a];
+      used[a] = true;
+      if (match[a] == -1) break;
+      a = p[match[a]];
+    }
+    while (true) {
+      b = base[b];
+      if (used[b]) return b;
+      b = p[match[b]];
+    }
+  }
+
+  void markPath(const vector<int> &base, vector<bool> &blossom, int v, int b, int children) {
+    for (; base[v] != b; v = p[match[v]]) {
+      blossom[base[v]] = blossom[base[match[v]]] = true;
+      p[v] = children;
+      children = match[v];
+    }
+  }
+  
+  int findPath(int root) {
+    vector<bool> used(n + 1);
+    vector<int> base(n + 1);
+    for_inc_range(u, 1, n) {
+      p[u] = -1;
+      base[u] = u;
+    }
+    used[root] = true;
+    
+    int qh = 0;
+    int qt = 0;
+    vector<int> q(n);
+    q[qt++] = root;
+    while (qh < qt) {
+      int v = q[qh++];
+      for (int to : g.getAdjacent(v)) {
+        if (base[v] == base[to] || match[v] == to) continue;
+        if (to == root || (match[to] != -1 && p[match[to]] != -1)) {
+          int curbase = lca(base, v, to);
+          vector<bool> blossom = vector<bool>(n + 1);
+          markPath(base, blossom, v, curbase, to);
+          markPath(base, blossom, to, curbase, v);
+          for_inc_range(i, 1, n) {
+            if (blossom[base[i]]) {
+              base[i] = curbase;
+              if (!used[i]) {
+                used[i] = true;
+                q[qt++] = i;
+              }
+            }
+          }
+        } else if (p[to] == -1) {
+          p[to] = v;
+          if (match[to] == -1)
+            return to;
+          to = match[to];
+          used[to] = true;
+          q[qt++] = to;
+        }
+      }
+    }
+    return -1;
+  }
 
 public:
-  BipartiteMatching(const BipartiteGraph &g) : g(g) {
-    nMatch = 0;
+  MaximumMatching(const UndirectedGraph &g): g(g) {
+    n = g.getSize();
+    match.resize(n + 1);
+    for_inc_range(u, 1, n) match[u] = -1;
+    p.resize(n + 1);
+    for_inc_range(u, 1, n) {
+      if (match[u] == -1) {
+        int v = findPath(u);
+        while (v != -1) {
+          int pv = p[v];
+          int ppv = match[pv];
+          match[v] = pv;
+          match[pv] = v;
+          v = ppv;
+        }
+      }
+    }
     
-    mx.resize(g.nLeft + 1);
-    for_inc_range(x, 1, g.nLeft) mx[x] = -1;
-    my.resize(g.nRight + 1);
-    for_inc_range(y, 1, g.nRight) my[y] = -1;
-    pre.resize(g.nRight + 1);
-
-    greedyMatch();
-    for_inc_range(x, 1, g.nLeft) if (mx[x] == -1) {
-      for_inc_range(y, 1, g.nRight) pre[y] = -1;
-      if (match(x)) {
+    nMatch = 0;
+    for_inc_range(u, 1, n) {
+      if (match[u] != -1) {
         ++nMatch;
       }
     }
   }
-
-  int getMaximumMatchingSize() const { return nMatch; }
+  
+  int getMaximumMatchingSize() const {
+    return nMatch;
+  }
 };
 
-#define BASE 300
-#define HMOD 100000000000000007LL
+#define BASE 31
+#define HMOD 1000001927
 
 char s[1010];
 
@@ -240,7 +364,7 @@ int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   //testGen();
-  freopen("biginput1.txt", "r", stdin);
+  freopen("input1.txt", "r", stdin);
 #endif
 
   while (1) {
@@ -257,7 +381,8 @@ int main() {
       rsh.push_back(StringHasher<BASE, HMOD>::getHash(ss.rbegin(), ss.rend()));
     }
 
-    BipartiteGraph g(n, n);
+    UndirectedGraph g;
+    g.init(n);
     for_inc(i, n) for_inc(j, i) {
       bool good = false;
       if (StringHasher<BASE, HMOD>::getHashValueConcat(rsh[j], rsh[i]) ==
@@ -273,11 +398,14 @@ int main() {
       if (good) {
         // LOG(1, i + 1 << " " << j + 1);
         g.addEdge(i + 1, j + 1);
-        g.addEdge(j + 1, i + 1);
       }
     }
-    BipartiteMatching matching(g);
-    cout << matching.getMaximumMatchingSize() / 2 << endl;
+
+    
+    MaximumMatching matching(g);
+    
+    cout << n - matching.getMaximumMatchingSize() / 2<< endl;
+    
   }
   return 0;
 }
