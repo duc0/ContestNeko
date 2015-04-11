0565bbc9ff6bb5cb2ee2f968ef386835e90eabe1
duc <ducnm0@gmail.com>
2015-04-11 11:31:17 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -1,13 +1,9 @@
-//#define SUBMIT
-
-#define C11
-
-#ifdef SUBMIT
 #define LOGLEVEL 0
 #define NDEBUG
-#else
+
+// BEGIN CUT HERE
 #define LOGLEVEL 1
-#endif
+// END CUT HERE
 
 #include <cstdio>
 #include <algorithm>
@@ -19,22 +15,18 @@
 #include <set>
 #include <cmath>
 #include <cstdlib>
+#include <array>
+#include <type_traits>
 #include <queue>
 #include <stack>
 #include <functional>
 #include <sstream>
+#include <unordered_set>
+#include <unordered_map>
 #include <deque>
 #include <climits>
 #include <cfloat>
-#include <bitset>
-
-#ifdef C11
-#include <array>
-#include <type_traits>
 #include <random>
-#include <unordered_set>
-#include <unordered_map>
-#endif
 
 using namespace std;
 
@@ -52,22 +44,216 @@ using namespace std;
 #define INT_INF ((int)2E9L)
 #define INT64_INF ((int64)1E18L)
 #define MOD 1000000007
-int MODP(int64 x) {
-  int r = x % MOD;
-  if (r < 0) r += MOD;
-  return r;
+
+#define countBit __builtin_popcount
+
+class Revmatching {
+public:
+  int smallest( vector <string> a ) {
+    int n = (int)a.size();
+    //hall's theorem
+    int ans = INT_INF;
+    for_inc(s, 1 << n) {
+      if (s == 0) continue;
+      
+      vector<int> adj;
+      
+      for_inc(j, n) {
+        int cost = 0;
+        for_inc(i, n) if (s & (1 << i)) {
+          cost += (a[i][j] - '0');
+        }
+        if (cost > 0) {
+          adj.push_back(cost);
+        }
+      }
+      
+      int sz = countBit(s);
+      if (adj.size() < sz) {
+        return 0;
+      }
+      
+      sort(adj.begin(), adj.end());
+      int cnt = (int)adj.size(), idx = 0, cost = 0;
+      
+      while (cnt >= sz) {
+        cost += adj[idx];
+        idx++;
+        cnt--;
+      }
+      
+      ans = min(ans, cost);
+    }
+    return ans;
+  }
+};
+
+// BEGIN CUT HERE
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace moj_harness {
+	using std::string;
+	using std::vector;
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			std::cerr << "No test cases run." << std::endl;
+		} else if (correct < total) {
+			std::cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << std::endl;
+		} else {
+			std::cerr << "All " << total << " tests passed!" << std::endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, std::clock_t elapsed) { 
+		std::cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			std::sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		std::cerr << verdict;
+		if (!info.empty()) {
+			std::cerr << " (";
+			for (size_t i=0; i<info.size(); ++i) {
+				if (i > 0) std::cerr << ", ";
+				std::cerr << info[i];
+			}
+			std::cerr << ")";
+		}
+		std::cerr << std::endl;
+		
+		if (verdict == "FAILED") {
+			std::cerr << "    Expected: " << expected << std::endl; 
+			std::cerr << "    Received: " << received << std::endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			string A[]                = {"1"};
+			int expected__            = 1;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			string A[]                = {"0"};
+			int expected__            = 0;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			string A[]                = {"44","44"};
+			int expected__            = 8;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			string A[]                = {"861","870","245"};
+			int expected__            = 6;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			string A[]                = {"01000","30200","11102","10001","11001"};
+			int expected__            = 0;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 5: {
+			string A[]                = {"0111101100","0001101001","1001001000","1000100001","0110011111","0011110100","1000001100","0001100000","1000100001","0101110010"};
+			int expected__            = 1;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 6: {
+			string A[]                = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			string A[]                = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 8: {
+			string A[]                = ;
+			int expected__            = ;
+
+			std::clock_t start__      = std::clock();
+			int received__            = Revmatching().smallest(vector <string>(A, A + (sizeof A / sizeof A[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
 }
 
-void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  fclose(stdout);
-}
 
-int main() {
-  ios::sync_with_stdio(false);
-#ifndef SUBMIT
-  //testGen();
-  freopen("input1.txt", "r", stdin);
-#endif
-  return 0;
+#include <cstdlib>
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(std::atoi(argv[i]));
+	}
 }
+// END CUT HERE
