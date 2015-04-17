2582abf0082c2c8a9c44cf30d3b65ea2e35b2e28
duc <ducnm0@gmail.com>
2015-04-17 09:58:33 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -66,11 +66,104 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAX 20
+
+bool notSame(int x1, int y1, int x2, int y2) {
+  return x1 != x2 || y1 != y2;
+}
+int firstWin[2][MAX + 1][MAX + 1][MAX + 1][MAX + 1];
+int solve(int playFirst, int x1, int y1, int x2, int y2) {
+  assert(notSame(x1, y1, 0, 0) || notSame(x2, y2, 0, 0));
+  
+  if (firstWin[playFirst][x1][y1][x2][y2] != -1) {
+    return firstWin[playFirst][x1][y1][x2][y2];
+  }
+  
+  if (playFirst == 0) {
+    if (x1 == 0 && y1 == 0) {
+      return firstWin[playFirst][x1][y1][x2][y2] = 1;
+    } else if (x2 == 0 && y2 == 0) {
+      return firstWin[playFirst][x1][y1][x2][y2] = 0;
+    }
+    if (x1 - 1 >= 0 && notSame(x1 - 1, y1, x2, y2)) {
+      if (solve(1 - playFirst, x1 - 1, y1, x2, y2)) {
+        return firstWin[playFirst][x1][y1][x2][y2] = 1;
+      }
+    }
+    if (y1 - 1 >= 0 && notSame(x1, y1 - 1, x2 , y2)) {
+      if (solve(1 - playFirst, x1, y1 - 1, x2, y2)) {
+        return firstWin[playFirst][x1][y1][x2][y2] = 1;
+      }
+    }
+    return firstWin[playFirst][x1][y1][x2][y2] = 0;
+  } else {
+    if (x1 == 0 && y1 == 0) {
+      return firstWin[playFirst][x1][y1][x2][y2] = 1;
+    } else if (x2 == 0 && y2 == 0) {
+      return firstWin[playFirst][x1][y1][x2][y2] = 0;
+    }
+    if (x2 - 1 >= 0 && notSame(x1, y1, x2 - 1, y2)) {
+      if (!solve(1 - playFirst, x1, y1, x2 - 1, y2)) {
+        return firstWin[playFirst][x1][y1][x2 - 1][y2] = 0;
+      }
+    }
+    if (y2 - 1 >= 0 && notSame(x1, y1, x2, y2 - 1)) {
+      if (!solve(1 - playFirst, x1, y1, x2, y2 - 1)) {
+        return firstWin[playFirst][x1][y1][x2][y2 - 1] = 0;
+      }
+    }
+    if (x2 - 1 >= 0 && y2 - 1 >= 0 && notSame(x1, y1, x2 - 1, y2 - 1)) {
+      if (!solve(1 - playFirst, x1, y1, x2 - 1, y2 - 1)) {
+        return firstWin[playFirst][x1][y1][x2 - 1][y2 - 1] = 0;
+      }
+    }
+    return firstWin[playFirst][x1][y1][x2][y2] = 1;
+  }
+}
+
+void solve() {
+  memset(firstWin, -1, sizeof(firstWin));
+  for_inc_range(x1, 0, MAX) for_inc_range(y1, 0, MAX) for_inc_range(x2, 0, MAX) for_inc_range(y2, 0, MAX) {
+    if (x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0) continue;
+    if (!notSame(x1, y1, x2, y2)) continue;
+    bool firstWin = false;
+    if (x1 <= x2 && y1 <= y2) {
+      firstWin = true;
+    } else if (x1 + y1 <= max(x2, y2)) {
+      firstWin = true;
+    }
+    if (solve(0, x1, y1, x2, y2)) {
+//      cout << x1 << " " << y1 << " " << x2 << " " << y2 << endl;
+      assert(firstWin);
+    } else {
+      assert(!firstWin);
+//      assert(x1 >= x2 || x2 >= y2);
+    }
+  }
+}
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   //testGen();
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  //solve();
+  
+  int x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  bool firstWin = false;
+  if (x1 <= x2 && y1 <= y2) {
+    firstWin = true;
+  } else if (x1 + y1 <= max(x2, y2)) {
+    firstWin = true;
+  }
+  if (firstWin) {
+    cout << "Polycarp";
+  } else {
+    cout << "Vasiliy";
+  }
+
+  
   return 0;
 }
