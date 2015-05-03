1af41ee32e85c04e1fdd1f66c86c652e3dbcccb8
duc <ducnm0@gmail.com>
2015-05-03 10:15:03 -0700

    Add TLE sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -63,14 +63,90 @@ int MODP(int64 x) {
 
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
+  cout << 100 << endl;
+  repeat(100) {
+    int n = 200;
+    cout << n << endl;
+    vector<int> a(n + 1);
+    for_inc_range(i, 1, n) {
+      a[i] = i;
+    }
+    random_shuffle(a.begin() + 1, a.end());
+    for_inc_range(i, 1, n) {
+      cout << a[i] << " ";
+    }
+    cout << endl;
+  }
   fclose(stdout);
+}
+
+vector<int> f;
+int n;
+void apply(vector<int> &a) {
+  for_inc_range(i, 1, n) {
+    a[i] = f[a[i]];
+  }
 }
 
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   //testGen();
-  freopen("input1.txt", "r", stdin);
+  freopen("biginput1.txt", "r", stdin);
 #endif
+  
+  int nTest;
+  cin >> nTest;
+  while (nTest--) {
+  cin >> n;
+  
+  f.resize(n + 1);
+  for_inc_range(i, 1, n) {
+    cin >> f[i];
+  }
+  
+  vector<int> p1(n + 1), p2(n + 1);
+  for_inc_range(i, 1, n) {
+    p1[i] = i;
+    p2[i] = i;
+  }
+  
+  apply(p1);
+  apply(p2); apply(p2);
+  
+  int cnt = 0;
+  while (p1 != p2) {
+    cnt++;
+    apply(p1);
+    apply(p2); apply(p2);
+  }
+  
+  int start = 0;
+  for_inc_range(i, 1, n) {
+    p1[i] = i;
+  }
+  
+  while (p1 != p2) {
+    apply(p1);
+    apply(p2);
+    start++;
+  }
+  
+  int period = 1;
+  p2 = p1;
+  apply(p2);
+  while (p2 != p1) {
+    apply(p2);
+    period++;
+  }
+  
+  if ((start % period) != 0) {
+    start += (period - (start % period));
+  } else if (start == 0) {
+    start = period;
+  }
+  
+  cout << start << endl;
+  }
   return 0;
 }
