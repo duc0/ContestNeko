3fe807ac320db2fbe4e7e7c55b0d6d231a5249cf
duc <ducnm0@gmail.com>
2015-05-07 11:03:42 -0700

    Sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -66,11 +66,84 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAXN 3030
+int n, m;
+
+vector<int> adj[MAXN];
+
+int dist[MAXN][MAXN];
+
+int s1, t1, l1, s2, t2, l2;
+
+void bfs(int start) {
+  queue<int> q;
+  
+  q.push(start);
+  
+  dist[start][start] = 0;
+  
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
+    int curDist = dist[start][cur];
+    
+    for (auto &next: adj[cur]) {
+      if (dist[start][next] == -1) {
+        dist[start][next] = curDist + 1;
+        q.push(next);
+      }
+    }
+  }
+}
+
+int solve() {
+  int ans = dist[s1][t1] + dist[s2][t2];
+  for_inc_range(mid1, 1, n) {
+    for_inc_range(mid2, 1, n) {
+      int time1 = dist[s1][mid1] + dist[mid1][mid2] + dist[mid2][t1];
+      int time2 = dist[s2][mid1] + dist[mid1][mid2] + dist[mid2][t2];
+      if (time1 <= l1 && time2 <= l2) {
+        int totalEdge = time1 + time2 - dist[mid1][mid2];
+        ans = min(ans, totalEdge);
+      }
+    }
+  }
+  return ans;
+}
+
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   //testGen();
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  cin >> n >> m;
+  
+  repeat(m) {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  
+  memset(dist, -1, sizeof(dist));  
+  for_inc_range(u, 1, n) {
+    bfs(u);
+  }
+  
+  cin >> s1 >> t1 >> l1 >> s2 >> t2 >> l2;
+  
+  if (dist[s1][t1] > l1 || dist[s2][t2] > l2) {
+    cout << -1 << endl;
+    return 0;
+  }
+  
+  int ans1 = solve();
+  swap(s2, t2);
+  int ans2 = solve();
+  int ans = min(ans1, ans2);
+  
+  cout << m - ans << endl;
   return 0;
 }
