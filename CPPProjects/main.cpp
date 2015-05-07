64e8cbc8b9f41ff534b52d8aba8477be1ef91c12
duc <ducnm0@gmail.com>
2015-05-07 09:57:27 -0700

    Sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -54,7 +54,7 @@ using namespace std;
 #define fill0(x) memset(x, 0, sizeof(x))
 #define INT_INF ((int)2E9L)
 #define INT64_INF ((int64)1E18L)
-#define MOD 1000000007
+int MOD;
 int MODP(int64 x) {
   int r = x % MOD;
   if (r < 0) r += MOD;
@@ -66,11 +66,50 @@ void testGen() {
   fclose(stdout);
 }
 
+#define MAX 550
+
+int f[MAX][MAX];
+int bugPerLine[MAX];
+
 int main() {
   ios::sync_with_stdio(false);
 #ifndef SUBMIT
   //testGen();
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  int nProg, nLine, maxBug;
+  cin >> nProg >> nLine >> maxBug >> MOD;
+  
+  if (MOD == 1) {
+    cout << 0 << endl;
+    return 0;
+  }
+  
+  for_inc_range(i, 1, nProg) {
+    cin >> bugPerLine[i];
+  }
+  
+  int cur = 0, next = 1;
+  memset(f, 0, sizeof(f));
+  
+  for_inc_range(bug, 0, maxBug) {
+    f[0][bug] = 1 % MOD;
+  }
+  
+  for_inc_range(idx, 1, nProg) {
+    
+    for_inc_range(line, 1, nLine) {
+      for_inc_range(bug, bugPerLine[idx], maxBug) {
+        f[line][bug] = (f[line][bug] + f[line - 1][bug - bugPerLine[idx]]) % MOD;
+        //LOG(1, line << " " << bug << " " << f[line][bug]);
+      }
+    }
+    
+    cur = 1 - cur;
+    next = 1 - next;
+  }
+  
+  cout << f[nLine][maxBug] << endl;
   return 0;
 }
