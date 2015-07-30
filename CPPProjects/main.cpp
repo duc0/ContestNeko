b75449638281c853b10b5bfcc1527e3915a54628
duc <ducnm0@gmail.com>
2015-07-30 09:41:43 -0700

    +Sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -1,76 +1,57 @@
-//#define SUBMIT
-
-#define C11
-
-#ifdef SUBMIT
-#define LOGLEVEL 0
-#define NDEBUG
-#else
-#define LOGLEVEL 1
-#endif
-
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
-#include <cassert>
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <cmath>
-#include <cstdlib>
-#include <queue>
-#include <stack>
-#include <functional>
-#include <sstream>
-#include <deque>
-#include <climits>
-#include <cfloat>
-#include <bitset>
-
-#ifdef C11
-#include <array>
-#include <type_traits>
-#include <random>
-#include <unordered_set>
-#include <unordered_map>
-#endif
-
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<math.h>
+#include<vector>
+#include<set>
+#include<stdlib.h>
+#include<string.h>
+#include<queue>
+#include<stack>
+#include<assert.h>
+#include<limits.h>
+#define tr(i) for(typeof(i.begin()) it=i.begin(); it!=i.end();it++)
+#define pb push_back
+#define mp make_pair
+#define REP(i,n) for(int i=0;i<n;i++)
+#define rep(i,s,n) for(int i=s;i<n;i++)
+#define s(n) scanf("%d",&n)
+#define XX first
+#define X first
+#define Y second
+#define all(a) a.begin(),a.end()
+#define YY second.first
+#define ZZ second.second
+#define fill(a,b) memset(a,b,sizeof(a))
+#define DREP(a) sort(all(a)); a.erase(unique(all(a)),a.end());
+#define INDEX(arr,ind) (lower_bound(all(arr),ind)-arr.begin())
+#define SZ(x) (int)(x.size())
+#define lin(val,j) (DP[j]-val*D[j])
 using namespace std;
-
-#define LOG(l, x) if (l <= LOGLEVEL) cout << x << endl
-
-#define int64 long long
-#define repeat(x) for (auto repeat_var = 0; repeat_var < x; ++repeat_var)
-
-#define for_inc(i, x) for (auto i = 0; i < x; ++i)
-#define for_dec(i, x) for (auto i = x - 1; i >= 0; --i)
-#define for_inc_range(i, x, y) for (auto i = x; i <= y; ++i)
-#define for_dec_range(i, x, y) for (auto i = x; i >= y; --i)
-
-#define countBit __builtin_popcount
-#define countBit64 __builtin_popcountl
-
-#define fill0(x) memset(x, 0, sizeof(x))
-#define INT_INF ((int)2E9L)
-#define INT64_INF ((int64)1E18L)
-#define MOD 1000000007
-int MODP(int64 x) {
-  int r = x % MOD;
-  if (r < 0) r += MOD;
-  return r;
-}
-
-void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  fclose(stdout);
-}
-
-int main() {
-  ios::sync_with_stdio(false);
-#ifndef SUBMIT
-  //testGen();
-  freopen("input1.txt", "r", stdin);
-#endif
+typedef long long LL;
+typedef pair<int,int> pii;
+typedef pair<int,pii> piii;
+#define MN 1000000
+int a[MN];
+int pos[MN+10];
+int DP[MN];
+int main()
+{
+  int n; s(n);
+  REP(i,n) s(a[i]);
+  int mx=1;
+  REP(i,MN+10) pos[i]=-1;
+  for(int i=n-1;i>=0;i--)
+  {
+    DP[i]=1;
+    for(int j=2;(a[i]*j)<=MN;j++)
+    {
+      if(pos[j*a[i]]==-1) continue;
+      DP[i]=max(DP[i],1+DP[pos[j*a[i]]]);
+    }
+    mx=max(mx,DP[i]);
+    pos[a[i]]=i;
+  }
+  cout<<mx<<endl;
   return 0;
 }
