8357643fc34f8fdf2bbba2c3e5b7c665f8bb1613
duc <ducnm0@gmail.com>
2015-08-01 09:55:09 -0700

    Case 7 timed out

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -110,7 +110,7 @@ namespace moj_harness {
 	using std::string;
 	using std::vector;
 	int run_test_case(int);
-	void run_test(int casenum = -1, bool quiet = false) {
+	void run_test(int casenum = 7, bool quiet = false) {
 		if (casenum != -1) {
 			if (run_test_case(casenum) == -1 && !quiet) {
 				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
@@ -242,6 +242,16 @@ namespace moj_harness {
         int B                     = 934233249;
         int K                     = 1090002004;
         int expected__            = 92445112;
+        
+        std::clock_t start__      = std::clock();
+        int received__            = BearPlays().pileSize(A, B, K);
+        return verify_case(casenum__, expected__, received__, clock()-start__);
+      }
+      case 7: {
+        int A                     = 333234427;
+        int B                     = 834233242;
+        int K                     = 1290000004;
+        int expected__            = 539127008;
         
         std::clock_t start__      = std::clock();
         int received__            = BearPlays().pileSize(A, B, K);
