935bfe454bf88ed52809bcc632e0417813ca0e18
duc <ducnm0@gmail.com>
2015-08-10 10:16:57 -0700

    +Sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -61,6 +61,177 @@ int MODP(int64 x) {
   return r;
 }
 
+template <class T> class NumberTheory {
+  static void extendedEuclid(T a, T b, T &x, T &y) {
+    if (b == 0) {
+      x = 1;
+      y = 0;
+      return;
+    }
+    T x2;
+    extendedEuclid(b, a % b, x2, x);
+    y = x2 - (a / b) * x;
+  }
+  
+public:
+  static inline T modulo(int64 a, T b) {
+    T r = a % b;
+    if (r < 0)
+      r += b;
+    return r;
+  }
+  static inline T modularInverse(T a, T m) {
+    T x, y;
+    extendedEuclid(a, m, x, y);
+    return modulo(x, m);
+  }
+};
+
+template <class T, T M> class ModInt {
+  T x = 0;
+  static inline T get(ModInt x) { return x.get(); }
+  static inline T get(T x) { return x; }
+  
+public:
+  ModInt() : ModInt(0) {}
+  ModInt(int64 y) { x = NumberTheory<T>::modulo(y, M); }
+  T get() const { return x; }
+  template <class Q> ModInt operator+(const Q &y) const {
+    return ModInt(x + get(y));
+  }
+  template <class Q> ModInt& operator+=(const Q &y) {
+    x = NumberTheory<T>::modulo(x + get(y), M);
+    return *this;
+  }
+  template <class Q> ModInt operator-(const Q &y) const {
+    return ModInt(x - get(y));
+  }
+  template <class Q> ModInt& operator-=(const Q &y) {
+    x = NumberTheory<T>::modulo(x - get(y), M);
+    return *this;
+  }
+  template <class Q> bool operator!=(const Q &y) const {
+    return x != get(y);
+  }
+  template <class Q> ModInt operator*(const Q &y) const {
+    return ModInt((int64)x * get(y));
+  }
+  template <class Q> ModInt& operator*=(const Q &y) {
+    x = NumberTheory<T>::modulo((int64)x * get(y), M);
+    return *this;
+  }
+  template <class Q> ModInt operator/(const Q &y) const {
+    return ModInt(
+                  (int64)x * NumberTheory<T>::modularInverse(get(y), MOD));
+  }
+  template <class Q> ModInt& operator/=(const Q &y) {
+    x = NumberTheory<T>::modulo((int64)x * NumberTheory<T>::modularInverse(get(y), MOD), M);
+    return *this;
+  }
+  ModInt &operator=(const T &y) {
+    x = NumberTheory<T>::modulo(y, M);
+    return *this;
+  }
+  ModInt &operator=(const ModInt &y) {
+    x = y.x;
+    return *this;
+  }
+  template<class Q> bool operator ==(const Q &y) const {
+    return x == get(y);
+  }
+  template<class Q> bool operator > (const Q &y) const {
+    return x > get(y);
+  }
+  template<class Q> bool operator >= (const Q &y) const {
+    return x >= get(y);
+  }
+  template<class Q> bool operator < (const Q &y) const {
+    return x < get(y);
+  }
+  template<class Q> bool operator <= (const Q &y) const {
+    return x <= get(y);
+  }
+  friend std::ostream& operator<< (std::ostream& stream, const ModInt& y) {
+    stream << get(y);
+    return stream;
+  }
+  
+};
+
+template<class T> class ComboUtils {
+  vector<T> factorial;
+  int maxN;
+public:
+  int getMaxN() const {
+    return maxN;
+  }
+  
+  ComboUtils(int n) {
+    this->maxN = n;
+    factorial.resize(n + 1);
+    factorial[0] = 1;
+    for_inc_range(i, 1, n) {
+      factorial[i] = factorial[i - 1] * i;
+    }
+  }
+  
+  // Number of ways to choose k objects from n objects
+  T C(int n, int k) const {
+    assert (0 <= n && n <= maxN);
+    if (k < 0 || k > n) return 0;
+    return factorial[n] / factorial[k] / factorial[n - k];
+  }
+  
+  // Number of ways to choose k objects from n objects and ordering is important
+  T A(int n, int k) const {
+    assert (0 <= n && n <= maxN);
+    if (k < 0 || k > n) return 0;
+    return factorial[n] / factorial[n - k];
+  }
+  
+  // Number of ways to arrange n objects
+  T P(int n) const {
+    assert (0 <= n && n <= maxN);
+    return factorial[n];
+  }
+  
+  // Number of ways to choose k objects from n objects, with repetition
+  T repeatC(int n, int k) const {
+    return C(n + k - 1, k);
+  }
+  
+  // Return a vector c[i] = C(i, k) for i <= n, O(n)
+  static vector<T> getCombByK(int n, int k) {
+    vector<T> c(n + 1);
+    c[k] = 1;
+    for_inc_range(i, k + 1, n) {
+      c[i] = c[i - 1] * i / (i - k);
+    }
+    return c;
+  }
+  
+  // Return a vector c[i] = C(n, i) for i <= n, O(n)
+  static vector<T> getCombByN(int n) {
+    vector<T> c(n + 1);
+    c[0] = 1;
+    for_inc_range(i, 1, n) {
+      c[i] = c[i - 1] * (n - i + 1)/ i;
+    }
+    return c;
+  }
+  
+  // Return a vector p[i] = a^i for i <= n, O(n)
+  static vector<T> getPower(int n, T a) {
+    vector<T> p(n + 1);
+    p[0] = 1;
+    for_inc_range(i, 1, n) {
+      p[i] = p[i - 1] * a;
+    }
+    return p;
+  }
+};
+
+
 void testGen() {
   freopen("biginput1.txt", "w", stdout);
   fclose(stdout);
@@ -72,5 +243,20 @@ int main() {
   //testGen();
   freopen("input1.txt", "r", stdin);
 #endif
+  
+  int n;
+  cin >> n;
+  n++;
+  vector<ModInt<int, MOD>> bell(n + 1);
+  bell[0] = 1;
+  ComboUtils<ModInt<int, MOD>> combo(n);
+  for_inc_range(i, 0, n - 1) {
+    bell[i + 1] = 0;
+    vector<ModInt<int, MOD>> c = combo.getCombByN(i);
+    for_inc_range(k, 0, i) {
+      bell[i + 1] = bell[i + 1] + c[k] * bell[k];
+    }
+  }
+  cout << bell[n] - bell[n - 1] << endl;
   return 0;
 }
