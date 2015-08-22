8d5f9bf00c511d4648162cecd30342533b1c703c
duc <ducnm0@gmail.com>
2015-08-22 00:20:04 -0700

    Add sol

--- CPPProjects/main.cpp
+++ CPPProjects/main.cpp
@@ -1,13 +1,9 @@
-//#define SUBMIT
-
-#define C11
-
-#ifdef SUBMIT
 #define LOGLEVEL 0
 #define NDEBUG
-#else
+
+// BEGIN CUT HERE
 #define LOGLEVEL 1
-#endif
+// END CUT HERE
 
 #include <cstdio>
 #include <algorithm>
@@ -19,22 +15,18 @@
 #include <set>
 #include <cmath>
 #include <cstdlib>
+#include <array>
+#include <type_traits>
 #include <queue>
 #include <stack>
 #include <functional>
 #include <sstream>
+#include <unordered_set>
+#include <unordered_map>
 #include <deque>
 #include <climits>
 #include <cfloat>
-#include <bitset>
-
-#ifdef C11
-#include <array>
-#include <type_traits>
 #include <random>
-#include <unordered_set>
-#include <unordered_map>
-#endif
 
 using namespace std;
 
@@ -48,29 +40,189 @@ using namespace std;
 #define for_inc_range(i, x, y) for (auto i = x; i <= y; ++i)
 #define for_dec_range(i, x, y) for (auto i = x; i >= y; --i)
 
-#define countBit __builtin_popcount
-#define countBit64 __builtin_popcountl
-
 #define fill0(x) memset(x, 0, sizeof(x))
 #define INT_INF ((int)2E9L)
 #define INT64_INF ((int64)1E18L)
 #define MOD 1000000007
-int MODP(int64 x) {
-  int r = x % MOD;
-  if (r < 0) r += MOD;
-  return r;
+
+class BallsInBoxes {
+public:
+  int64 maxTurns(int64 n, int64 k ) {
+    if (k == 1) {
+      return n - 1;
+    } else if (k == n) {
+      return 0;
+    }
+    int64 l = n - k + 1;
+    if (l % 2 == 0) {
+      int64 t = l / 2;
+      int64 newN = k + t - 1;
+      return 1 + maxTurns(newN, k);
+    } else {
+      int64 t = l / 2;
+      int64 newN = k + t - 1;
+      return 1 + max(maxTurns(newN, k), maxTurns(newN + 1, k));
+    }
+  }
+};
+
+// BEGIN CUT HERE
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace moj_harness {
+	using std::string;
+	using std::vector;
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			std::cerr << "No test cases run." << std::endl;
+		} else if (correct < total) {
+			std::cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << std::endl;
+		} else {
+			std::cerr << "All " << total << " tests passed!" << std::endl;
+		}
+	}
+	
+	int verify_case(int casenum, const long long &expected, const long long &received, std::clock_t elapsed) { 
+		std::cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			std::sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		std::cerr << verdict;
+		if (!info.empty()) {
+			std::cerr << " (";
+			for (size_t i=0; i<info.size(); ++i) {
+				if (i > 0) std::cerr << ", ";
+				std::cerr << info[i];
+			}
+			std::cerr << ")";
+		}
+		std::cerr << std::endl;
+		
+		if (verdict == "FAILED") {
+			std::cerr << "    Expected: " << expected << std::endl; 
+			std::cerr << "    Received: " << received << std::endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			long long N               = 10;
+			long long K               = 10;
+			long long expected__      = 0;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			long long N               = 100;
+			long long K               = 1;
+			long long expected__      = 99;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			long long N               = 1000;
+			long long K               = 999;
+			long long expected__      = 1;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			long long N               = 10;
+			long long K               = 5;
+			long long expected__      = 3;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 4: {
+			long long N               = ;
+			long long K               = ;
+			long long expected__      = ;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 5: {
+			long long N               = ;
+			long long K               = ;
+			long long expected__      = ;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 6: {
+			long long N               = ;
+			long long K               = ;
+			long long expected__      = ;
+
+			std::clock_t start__      = std::clock();
+			long long received__      = BallsInBoxes().maxTurns(N, K);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
 }
 
-void testGen() {
-  freopen("biginput1.txt", "w", stdout);
-  fclose(stdout);
-}
 
-int main() {
-  ios::sync_with_stdio(false);
-#ifndef SUBMIT
-  //testGen();
-  freopen("input1.txt", "r", stdin);
-#endif
-  return 0;
+#include <cstdlib>
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(std::atoi(argv[i]));
+	}
 }
+// END CUT HERE
